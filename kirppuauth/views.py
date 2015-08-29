9b693f66e98387cdfc2eb11df08098749736d510
Jyrki Launonen <jyrki.launonen@gmail.com>
2015-08-29 16:17:32 +0300

    Add error details to kirppuauth login form.
    - Add error handling to sudo.
    - Prefill previously entered username.

--- kirppuauth/views.py
+++ kirppuauth/views.py
@@ -7,6 +7,7 @@ from django.http import Http404
 from django.http.response import HttpResponseForbidden
 from django.shortcuts import redirect
 from django.shortcuts import render
+from django.utils.translation import ugettext as _
 from django.views.decorators.cache import never_cache
 from django.views.decorators.csrf import csrf_protect
 
@@ -19,17 +20,26 @@ def local_admin_login(request):
     if not (settings.KIRPPU_SU_AS_USER and request.user.is_authenticated() and request.user.is_superuser):
         raise Http404()
 
+    error = None
     if request.method == "POST":
         username = request.POST.get("username", "")
         if username != "":
-            user = get_user_model().objects.get(username=username)
-            if user.is_superuser:
-                return HttpResponseForbidden()
-            user.backend = 'django.contrib.auth.backends.ModelBackend'
-            logout(request)
-            login(request, user)
-            return redirect(reverse('kirppu:vendor_view'))
+            UserModel = get_user_model()
+            try:
+                user = UserModel.objects.get(username=username)
+            except UserModel.DoesNotExist:
+                error = _("User {} not found.").format(username)
+            else:
+                if user.is_superuser:
+                    return HttpResponseForbidden()
+                user.backend = 'django.contrib.auth.backends.ModelBackend'
+                logout(request)
+                login(request, user)
+                return redirect(reverse('kirppu:vendor_view'))
+        else:
+            error = _("Username is required.")
 
     return render(request, "kirppuauth/login.html", {
         "submit": reverse('kirppuauth:local_admin_login'),
+        "plain_error": error,
     })
