39a5d97439a91ebbc41b053fc313330c6279ae22
Jyrki Launonen <jyrki.launonen@gmail.com>
2015-10-11 10:51:04 +0300

    Add watch task for gulp.
    - Clarify missing pipeline configuration message.

--- kirppu/gulpfile.js
+++ kirppu/gulpfile.js
@@ -46,11 +46,17 @@ var fileHeader = function(header) {
     };
 };
 
+var handleError = function(err) {
+    gutil.log(gutil.colors.red("Error: ") + err);
+    return this.emit('end');
+};
+
 var jsTasks = _.map(pipeline.js, function(def, name) {
     var taskName = "js:" + name;
     gulp.task(taskName, function() {
         return gulp.src(srcPrepend(def))
             .pipe(gif(/\.coffee$/, coffee(), gutil.noop()))
+            .on('error', handleError)
             .pipe(concat(def.output_filename, {process: fileHeader(jsHeader)}))
             .pipe(gif(shouldCompress && def.compress, uglify()))
             .pipe(gulp.dest(DEST + "/js/"));
@@ -63,6 +69,7 @@ var cssTasks = _.map(pipeline.css, function(def, name) {
     gulp.task(taskName, function() {
         return gulp.src(srcPrepend(def))
             .pipe(concat(def.output_filename, {process: fileHeader(cssHeader)}))
+            .on('error', handleError)
             .pipe(gif(shouldCompress, minify()))
             .pipe(gulp.dest(DEST + "/css/"));
     });
@@ -95,14 +102,18 @@ var findTask = function(haystack, file) {
 };
 
 /**
- * Start task by --file commandline argument.
+ * Start task by watch or --file commandline argument.
  *
  * @param group {string} Pipeline group container name to try.
+ * @param file {string?} Optional file argument. If not supplied, environment is used.
  * @returns {boolean} True if task was run. Otherwise false.
  */
-var startFileTask = function(group) {
+var startFileTask = function(group, file) {
+    if (file === undefined) {
+        file = gutil.env.file;
+    }
     // Replace '\' with '/' so Windows file paths work.
-    var filename = gutil.env.file.replace(/\\/g, "/");
+    var filename = file.replace(/\\/g, "/");
     var task = findTask(_.result(pipeline, group), filename);
     if (task != null) {
         gulp.start(group + ":" + task);
@@ -117,6 +128,25 @@ gulp.task("build", function() {
         gutil.log(gutil.colors.red("Need argument: --file FILE"));
     }
     else if (!(startFileTask("css") || startFileTask("js"))) {
-        gutil.log(gutil.colors.red("No target found in pipeline.js: " + gutil.env.file));
+        gutil.log(gutil.colors.red("Target file not found in pipeline.js: " + file));
     }
+});
+
+/**
+ * Watcher function for gulp.watch. This will start file task for changed files.
+ */
+var watcher = function(event) {
+    var file = event.path;
+    if (event.type != "changed") {
+        // "added" / "deleted"
+        gutil.log("Unhandled event: " + event.type + " " + file);
+        return;
+    }
+    if (!(startFileTask("css", file) || startFileTask("js", file))) {
+        gutil.log(gutil.colors.red("Target file not found in pipeline.js: " + file));
+    }
+};
+
+gulp.task("watch", function() {
+    gulp.watch(SRC + "/**/*", watcher);
 });
