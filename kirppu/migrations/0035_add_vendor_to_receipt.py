kcsry/kirppu

commit aeb8625cf14435bae4921f7cec187891e187b00b

Rework provision calculation.
- Flip assertEquals arguments to correct order.
- Let compensable_items return provision information even if there are
no items to compensate.
- Add vendor to compensation Receipts to ensure they remain accessible
even without compensated items.

kirppu/migrations/0035_add_vendor_to_receipt.py
@@ -1,0 +1,36 @@
+from django.db import migrations, models
+import django.db.models.deletion
+
+
+# noinspection PyPep8Naming
+def detect_provision(apps, schema_editor):
+    db_alias = schema_editor.connection.alias
+    Receipt = apps.get_model("kirppu", "Receipt")
+    compensations = (
+        Receipt.objects
+        .filter(type="COMPENSATION")
+        .annotate(src_vendor=models.F("receiptitem__item__vendor_id"))
+    )
+    for c in compensations:
+        c.vendor_id = c.src_vendor
+        c.save(update_fields=("vendor",))
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('kirppu', '0034_provision_dsl'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='receipt',
+            name='vendor',
+            field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.CASCADE, to='kirppu.Vendor'),
+        ),
+        migrations.RunPython(detect_provision),
+        migrations.AddConstraint(
+            model_name='receipt',
+            constraint=models.CheckConstraint(check=models.Q(models.Q(('type', 'COMPENSATION'), ('vendor__isnull', False)), models.Q(models.Q(_negated=True, type='COMPENSATION'), ('vendor__isnull', True)), _connector='OR'), name='vendor_id_nullity'),
+        ),
+    ]

kirppu/checkout_api.py
@@ -501,8 +501,6 @@ def compensable_items(request, event, vendor):
     )
 
     items_for_compensation = vendor_items.filter(state=Item.SOLD)
-    if not items_for_compensation:
-        return {"items": []}
 
     r = dict(items=[i.as_dict() for i in items_for_compensation])
 
@@ -647,16 +645,19 @@ def item_compensate_start(request, event, vendor):
         raise AjaxError(RET_CONFLICT, _(u"Already compensating"))
 
     vendor_id = int(vendor)
-    if not Vendor.objects.filter(pk=vendor_id, event=event).exists():
+    vendor_list = list(Vendor.objects.filter(pk=vendor_id, event=event))
+    if not vendor_list:
         raise AjaxError(RET_BAD_REQUEST)
 
     clerk = Clerk.objects.get(pk=request.session["clerk"])
     counter = Counter.objects.get(pk=request.session["counter"])
 
-    receipt = Receipt()
-    receipt.clerk = clerk
-    receipt.counter = counter
-    receipt.type = Receipt.TYPE_COMPENSATION
+    receipt = Receipt(
+        clerk=clerk,
+        counter=counter,
+        type=Receipt.TYPE_COMPENSATION,
+        vendor=vendor_list[0]
+    )
     receipt.save()
 
     request.session["compensation"] = (receipt.pk, vendor_id)
@@ -1000,7 +1001,7 @@ def receipt_pending(request):
 def receipt_compensated(request, vendor):
     receipts = Receipt.objects.filter(
         type=Receipt.TYPE_COMPENSATION,
-        receiptitem__item__vendor_id=int(vendor),
+        vendor_id=int(vendor)
     ).distinct().order_by("start_time")
 
     return [receipt.as_dict() for receipt in receipts]

kirppu/models.py
@@ -1067,6 +1067,9 @@ class Receipt(models.Model):
     end_time = models.DateTimeField(null=True, blank=True)
     type = models.CharField(choices=TYPES, max_length=16, default=TYPE_PURCHASE)
 
+    # Relevant only for vendor-specific receipts, i.e. TYPE_COMPENSATION.
+    vendor = models.ForeignKey(Vendor, on_delete=models.CASCADE, null=True, blank=True)
+
     def items_list(self):
         return [
             self._item_dict(row)
@@ -1128,6 +1131,12 @@ class Receipt(models.Model):
         permissions = (
             ("view_accounting", "View accounting data"),
         )
+        constraints = [
+            models.CheckConstraint(check=(
+                models.Q(type="COMPENSATION", vendor__isnull=False)) | (
+                ~models.Q(type="COMPENSATION") & models.Q(vendor__isnull=True)),
+                name="vendor_id_nullity")
+        ]
 
 
 class ReceiptExtraRow(models.Model):

kirppu/provision.py
@@ -4,7 +4,7 @@ from typing import Optional
 
 from django.conf import settings
 from django.core.exceptions import SuspiciousOperation
-from django.db.models import Sum, Q
+from django.db.models import Sum, Q, QuerySet
 
 from .models import ReceiptExtraRow, Receipt, Item, ReceiptItem
 from .provision_dsl import run
@@ -13,7 +13,7 @@ __author__ = 'codez'
 
 
 class Provision(object):
-    def __init__(self, vendor_id, provision_function, receipt=None):
+    def __init__(self, vendor_id: int, provision_function: str, receipt=None):
         self._vendor_id = vendor_id
 
         self._vendor_items = vendor_items = Item.objects.filter(vendor__id=vendor_id)
@@ -30,38 +30,34 @@ class Provision(object):
         self._total_compensation_items = total_compensation_items
         self._receipt = receipt
 
-        self._sub_total = None
+        # Provision to be paid from current SOLD items.
         self._provision_result = None
+        # Provision to be paid extra from COMPENSATED items, i.e. fixing a mistake in calculation.
         self._provision_fix_result = None
 
         self._provision_function = provision_function
 
-        if not total_compensation_items:
-            self._provision = None
-        else:
-            self._provision = self.run_function(
-                self._provision_function,
-                total_compensation_items
-            )
+        # Total value of provision calculated from SOLD/receipt + COMPENSATED items.
+        self._total_provision = self._run_function()
 
-            if self._provision is not None:
-                if receipt is None:
-                    self._sum_for_compensation = \
-                        vendor_items.filter(state=Item.SOLD).aggregate(sum=Sum("price"))["sum"]
-                else:
-                    self._sum_for_compensation = \
-                        ReceiptItem.objects.filter(receipt=receipt, action=ReceiptItem.ADD) \
-                                           .aggregate(sum=Sum("item__price"))["sum"]
-                if self._sum_for_compensation is None:
-                    self._sum_for_compensation = Decimal(0)
+        if self.has_provision:
+            if receipt is None:
+                self._sum_for_compensation = \
+                    vendor_items.filter(state=Item.SOLD).aggregate(sum=Sum("price"))["sum"]
+            else:
+                self._sum_for_compensation = \
+                    ReceiptItem.objects.filter(receipt=receipt, action=ReceiptItem.ADD) \
+                                       .aggregate(sum=Sum("item__price"))["sum"]
+            if self._sum_for_compensation is None:
+                self._sum_for_compensation = Decimal(0)
 
     @property
     def has_provision(self):
-        return self._provision is not None
+        return self._total_provision is not None and self._total_provision != Decimal(0)
 
     @classmethod
     def run_function(cls, provision_function, sold_and_compensated) -> Optional[Decimal]:
-        if provision_function is None or provision_function == "":
+        if provision_function is None or provision_function.strip() in ("", "null", "0"):
             return None
         if not getattr(settings, "KIRPPU_ALLOW_PROVISION_FUNCTIONS", False):
             raise SuspiciousOperation("Provision functions are not allowed.")
@@ -71,6 +67,12 @@ class Provision(object):
         assert _r is None or isinstance(_r, Decimal), "Value returned from function must be null or a number"
         return _r
 
+    def _run_function(self, items: Optional[QuerySet] = None) -> Optional[Decimal]:
+        q = items
+        if items is None:
+            q = self._total_compensation_items
+        return self.run_function(self._provision_function, q)
+
     def _calculate_provision_information(self):
         """
 
@@ -78,39 +80,53 @@ class Provision(object):
         :rtype: (ReceiptExtraRow, ReceiptExtraRow)
         """
         extras = ReceiptExtraRow.objects.filter(
-            type=ReceiptExtraRow.TYPE_PROVISION,
+            type__in=(ReceiptExtraRow.TYPE_PROVISION, ReceiptExtraRow.TYPE_PROVISION_FIX),
             receipt__type=Receipt.TYPE_COMPENSATION,
             receipt__receiptitem__item__vendor_id=self._vendor_id,
         ).distinct()
         extras = extras.aggregate(extras_value=Sum("value"))
         previous_provisions = extras["extras_value"] or Decimal(0)
 
-        provision_now = -min(self._provision + previous_provisions, self._sum_for_compensation)
-        previous_items = self._vendor_items.filter(state=Item.COMPENSATED)
+        # [      Sold      ][ Compensated ]
+        # [provision_result]
+        # [         provision(All)        ]
+
+        # fix = fn(Compensated) - sum(receipts.provision*)
+        # provision(Sold) = provision(All) - fn(Compensated)
+
+        compensated_items = self._vendor_items.filter(state=Item.COMPENSATED)
         if self._receipt:
             # Due no state for staged for compensation, exclude current receipt from fix calculations.
-            previous_items = previous_items.exclude(receipt=self._receipt)
-        old_target_provision = self.run_function(self._provision_function, previous_items)
-        provision_fixup_value = -(old_target_provision + previous_provisions)
+            compensated_items = compensated_items.exclude(receipt=self._receipt)
+        fn_compensated = self._run_function(compensated_items)
 
-        assert provision_now <= provision_fixup_value, str(provision_now) + " " + str(provision_fixup_value)
-        self._provision_result = (provision_now - provision_fixup_value).quantize(Decimal(".01"))
-        self._provision_fix_result = provision_fixup_value.quantize(Decimal(".01"))
+        self._provision_fix_result = fn_compensated + previous_provisions  # previous_provisions is a negative value.
+        self._provision_result = self._total_provision - fn_compensated
 
-    @property
-    def sub_total(self):
-        if self._sub_total is None:
-            self._sub_total = self._vendor_items.aggregate(sum=Sum("price"))["sum"]
-        return self._sub_total
+        if not compensated_items.exists() and self._provision_fix_result != Decimal(0):
+            self._provision_result += self._provision_fix_result
+            self._provision_fix_result = Decimal(0)
+
+        # We present numbers in negative as the values are used in "how much would be given to vendor" context.
+        self._provision_result = -self._provision_result
+        self._provision_fix_result = -self._provision_fix_result
+
+    def _ensure_result(self):
+        if self._provision_result is None and self.has_provision:
+            self._calculate_provision_information()
 
     @property
     def provision(self):
-        if self._provision_result is None and self.has_provision:
-            self._calculate_provision_information()
+        """Provision to be paid (negative) from current SOLD/receipt items."""
+        self._ensure_result()
         return self._provision_result
 
     @property
     def provision_fix(self):
-        if self._provision_fix_result is None and self.has_provision:
-            self._calculate_provision_information()
+        """Fix value to be paid (negative) due rounding errors or function adjustment."""
+        self._ensure_result()
         return self._provision_fix_result
+
+    def __str__(self):
+        return "Provision(result={}, fix={}, total={})".format(
+            self.provision, self.provision_fix, self._total_provision)

kirppu/static_src/css/checkout.css
@@ -138,3 +138,16 @@ kbd.shortcut {
         transform: rotate(360deg);
     }
 }
+
+.warn-blink {
+    animation: warn-blink 0.5s steps(2, start) 20;
+}
+
+@keyframes warn-blink {
+    from {
+        color: #e82121;
+    }
+    to {
+        color: #333333;
+    }
+}

kirppu/static_src/js/checkout/vendorcompensation.coffee
@@ -35,7 +35,7 @@ class @VendorCompensation extends CheckoutMode
       onConfirm: if cfg.confirm then t.onConfirm
       onAbort: if cfg.abort then t.onCancel
       onContinue: if cfg.continue then t.onCancel else (if cfg.skip then t.onSkipFailed)
-      continueWarn: if cfg.skip then true
+      continueWarn: if cfg.skip or cfg.warn then true
       onRetry: if cfg.retry then t.onRetryFailed
     Template.vendor_compensation_buttons(cbs)
 
@@ -45,7 +45,7 @@ class @VendorCompensation extends CheckoutMode
 
     rows = @compensableItems.slice()
     compensableSum = @compensableItems.reduce(((acc, item) -> acc + item.price), 0)
-    provisionAmount = 0
+    @provisionAmount = 0
     if @compensableExtras?
       rows.push(
         action: "EXTRA"
@@ -55,20 +55,19 @@ class @VendorCompensation extends CheckoutMode
       )
       rows = rows.concat(@compensableExtras)
       for i in @compensableExtras
-        provisionAmount += i.value
-    else
-      provisionAmount = 0
+        @provisionAmount += i.value
 
 
-    if @compensableItems.length > 0
+    if rows.length > 0
+      total = compensableSum + @provisionAmount
       table = Template.item_report_table(
-        caption: gettext("Sold Items")
+        caption: if total >= 0 then gettext("Sold Items") else gettext("Balance Reconciliation")
         items: rows
-        sum: compensableSum + provisionAmount
+        sum: total
         extra_col: true
       )
       @itemDiv.empty().append(table)
-      @buttonForm.empty().append(@buttons(confirm: true, abort: true))
+      @buttonForm.empty().append(@buttons(confirm: true, abort: true, warn: total < 0))
 
     else
       @itemDiv.empty().append($('<em>').text(gettext('No compensable items')))
@@ -78,10 +77,10 @@ class @VendorCompensation extends CheckoutMode
 
   onConfirm: =>
     nItems = @compensableItems.length
-    if nItems == 0
+    if nItems == 0 and @provisionAmount == 0
       console.error("What? Nothing to compensate?")
       return
-    @_createProgress(nItems)
+    @_createProgress(Math.max(nItems, 1))
 
     # Add table row indices for items (needed to point to the rows at UI).
     for i, index in @compensableItems
@@ -90,7 +89,11 @@ class @VendorCompensation extends CheckoutMode
     Api.item_compensate_start(vendor: @vendor.id)
       .done(=>
         @_loopResult = []
-        @_loopBack(@compensableItems)
+        if nItems > 0
+          @_loopBack(@compensableItems)
+        else
+          @_setProgress(1)
+          @_onLoopDone()
       )
       .fail((jqXHR) =>
         safeAlert("Failed to start compensation: #{jqXHR.status}: #{jqXHR.responseText}")

kirppu/static_src/jst/item_report_table.jsx
@@ -1,10 +1,21 @@
 import {dateTime, yesNo, title} from "./helpers";
 
 function Total({sum, extra_col, hide_status}) {
+    let price
+    if (sum < 0 && !hide_status) {
+        price = [
+            <span className="glyphicon glyphicon-warning-sign warn-blink"
+                  title={gettext("Vendor must pay!")}/>
+            , " "
+            , displayPrice(sum)
+        ]
+    } else {
+        price = displayPrice(sum)
+    }
     return (
-        <tr>
+        <tr className={sum < 0 ? "danger" : ""}>
             <th colSpan="3">{gettext("Total:")}</th>
-            <th className="receipt_price numeric">{displayPrice(sum)}</th>
+            <th className="receipt_price numeric">{price}</th>
             {!hide_status && <th/>}
             {!hide_status && <th/>}
             {extra_col && <th className="receipt_extra_col"/>}

kirppu/static_src/jst/vendor_compensation_buttons.jsx
@@ -1,7 +1,7 @@
 export default function render({onConfirm, onAbort, onContinue, continueWarn, onRetry}) {
     const btns = [
         onConfirm &&
-        <input type="button" className={"btn btn-success"}
+        <input type="button" className={"btn " + (continueWarn ? "btn-warning" : "btn-success")}
                value={gettext('Confirm')}
                onclick={onConfirm}
         />

commit b197b060856ea454fd86159299dda08122e7dfaf

Split schema and data migration from add_vendor_to_receipt.
Broken in aeb8625cf14435bae4921f7cec187891e187b00b.

- Get less stuff from database for data migration.
- Fix dev_data.

kirppu/migrations/0035_add_vendor_to_receipt.py
@@ -10,6 +10,8 @@ def detect_provision(apps, schema_editor):
         Receipt.objects.using(db_alias)
         .filter(type="COMPENSATION")
         .annotate(src_vendor=models.F("receiptitem__item__vendor_id"))
+        .only("id")
+        .distinct()
     )
     for c in compensations:
         c.vendor_id = c.src_vendor
@@ -29,8 +31,4 @@ class Migration(migrations.Migration):
             field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.CASCADE, to='kirppu.Vendor'),
         ),
         migrations.RunPython(detect_provision),
-        migrations.AddConstraint(
-            model_name='receipt',
-            constraint=models.CheckConstraint(check=models.Q(models.Q(('type', 'COMPENSATION'), ('vendor__isnull', False)), models.Q(models.Q(_negated=True, type='COMPENSATION'), ('vendor__isnull', True)), _connector='OR'), name='vendor_id_nullity'),
-        ),
     ]

kirppu/migrations/0036_add_receipt_vendor_constraint.py
@@ -1,0 +1,15 @@
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('kirppu', '0035_add_vendor_to_receipt'),
+    ]
+
+    operations = [
+        migrations.AddConstraint(
+            model_name='receipt',
+            constraint=models.CheckConstraint(check=models.Q(models.Q(('type', 'COMPENSATION'), ('vendor__isnull', False)), models.Q(models.Q(_negated=True, type='COMPENSATION'), ('vendor__isnull', True)), _connector='OR'), name='vendor_id_nullity'),
+        ),
+    ]

kirppu/migrations/0037_adjust_clerk_managers.py
@@ -5,7 +5,7 @@ import django.db.models.manager
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('kirppu', '0035_add_vendor_to_receipt'),
+        ('kirppu', '0036_add_receipt_vendor_constraint'),
     ]
 
     operations = [

kirppu/migrations/0038_event_source_db.py
@@ -4,7 +4,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('kirppu', '0036_adjust_clerk_managers'),
+        ('kirppu', '0037_adjust_clerk_managers'),
     ]
 
     operations = [