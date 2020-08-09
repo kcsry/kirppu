b6937405a9b85026f3e9cffc94fa65c87ee793c0
Jyrki Launonen <jyrki.launonen@gmail.com>
2020-08-09 11:53:42 +0300

    Add possibility to debug csv streamer views.

--- kirppu/views/csv_utils.py
+++ kirppu/views/csv_utils.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 import functools
+import html
 import io
 from urllib.parse import quote
 
-
-from django.http import StreamingHttpResponse
+from django.conf import settings
+from django.http import HttpResponse, StreamingHttpResponse
 
 
 def strip_generator(fn):
@@ -20,15 +21,28 @@ def strip_generator(fn):
 
 
 def csv_streamer_view(request, generator, filename_base):
+    debug = settings.DEBUG and request.GET.get("debug") is not None
+
     def streamer():
+        if debug:
+            yield "<!DOCTYPE html>\n<html>\n<body>\n<pre>"
         output = io.StringIO()
         for a_string in generator(output):
             val = output.getvalue()
-            yield val
+            if debug:
+                yield html.escape(val, quote=False)
+            else:
+                yield val
             output.truncate(0)
             output.seek(0)
+        if debug:
+            yield "</pre>\n</body>\n</html>"
 
-    response = StreamingHttpResponse(streamer(), content_type="text/plain; charset=utf-8")
+    if debug:
+        response = HttpResponse("".join(streamer()))
+    else:
+        response = StreamingHttpResponse(streamer(), content_type="text/plain; charset=utf-8")
+
     if request.GET.get("download") is not None:
         response["Content-Disposition"] = 'attachment; filename="%s.csv"' % quote(filename_base, safe="")
         response["Content-Type"] = "text/csv; charset=utf-8"
