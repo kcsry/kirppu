3136f7e37b339252d4c1f5642974e180070c452d
Jyrki Launonen <jyrki.launonen@gmail.com>
2020-08-10 19:01:44 +0300

    Allow migrations to be run on extra databases.
    - Remove duplicate registration of save and delete signals. Already
    registered in apps.

--- kirppu/signals.py
+++ kirppu/signals.py
@@ -1,18 +1,30 @@
 # -*- coding: utf-8 -*-
 
-from django.db.models.signals import pre_save, pre_delete
+from django.db.models.signals import pre_migrate, post_migrate
 from django.dispatch import receiver
 
+ENABLE_CHECK = True
 
-@receiver(pre_save)
+
+@receiver(pre_migrate)
+def pre_migrate_handler(*args, **kwargs):
+    global ENABLE_CHECK
+    ENABLE_CHECK = False
+
+
+@receiver(post_migrate)
+def post_migrate_handler(*args, **kwargs):
+    global ENABLE_CHECK
+    ENABLE_CHECK = True
+
+
 def save_handler(sender, instance, using, **kwargs):
     # noinspection PyProtectedMember
-    if instance._meta.app_label in ("kirppu", "kirppuauth") and using != "default":
+    if ENABLE_CHECK and instance._meta.app_label in ("kirppu", "kirppuauth") and using != "default":
         raise ValueError("Saving objects in non-default database should not happen")
 
 
-@receiver(pre_delete)
 def delete_handler(sender, instance, using, **kwargs):
     # noinspection PyProtectedMember
-    if instance._meta.app_label in ("kirppu", "kirppuauth") and using != "default":
+    if ENABLE_CHECK and instance._meta.app_label in ("kirppu", "kirppuauth") and using != "default":
         raise ValueError("Deleting objects from non-default database should not happen")
