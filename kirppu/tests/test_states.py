kcsry/kirppu

commit 87629cdcd9a4fbed3a902e25a7f8880121bc5302

Make possible to change box price via overseer item edit.
Show prices and counts per item state and price in the dialog, for
reference.
When saving, change only available box items' prices.

- Change vendor report and mobile view to display the boxes having
  differently priced items as separate rows.

kirppu/api/boxes_api.py
@@ -10,7 +10,7 @@ from .common import (
 from ..ajax_util import AjaxError, RET_BAD_REQUEST, RET_CONFLICT
 from ..checkout_api import ajax_func
 from ..forms import remove_item_from_receipt
-from ..models import Item, ItemStateLog, ReceiptItem
+from ..models import Item, ItemStateLog, ReceiptItem, decimal_to_transport
 
 __author__ = 'codez'
 
@@ -91,7 +91,14 @@ def box_item_reserve(request, event, box_number, box_item_count="1"):
     # Must force id-list to ensure stability.
     # Otherwise the "list" is considered as a subquery which may not be stable.
     candidates = list(box.item_set.select_for_update()
-                      .filter(state=Item.BROUGHT)[:box_item_count].values_list("pk", flat=True))
+                      .filter(state=Item.BROUGHT)[:box_item_count + 1].values_list("pk", flat=True))
+
+    # Avoid representative item, as it is needed for representing all available items.
+    # Reserve it only when it is last item to be reserved.
+    representative_item_id = box.representative_item_id
+    if len(candidates) == box_item_count + 1:
+        candidates = [c for c in candidates if c != representative_item_id]
+
     if len(candidates) == box_item_count:
         items = Item.objects.filter(pk__in=candidates)
         rows = [
@@ -109,12 +116,19 @@ def box_item_reserve(request, event, box_number, box_item_count="1"):
         receipt.calculate_total()
         receipt.save(update_fields=("total",))
 
+        result_items = [
+            {
+                "code": entry["code"],
+                "price": decimal_to_transport(entry["price"]),
+            }
+            for entry in items.values("code", "price")
+        ]
         ret = box.as_dict()
+        del ret["item_price"]  # item_price assumes representative item has same price as ones being reserved.
         ret.update(
-            price=ret["item_price"],
             total=receipt.total_cents,
             changed=box_item_count,
-            item_codes=list(items.values_list("code", flat=True)),
+            items=result_items,
             item_name=box.representative_item.name,
         )
         return ret
@@ -133,23 +147,36 @@ def box_item_release(request, event, box_number, box_item_count="1"):
         raise AjaxError(RET_BAD_REQUEST, "No active receipt found")
     receipt = get_receipt(receipt_id, for_update=True)
 
-    box_items = receipt.items.select_for_update().filter(receiptitem__action=ReceiptItem.ADD, box=box)[:box_item_count]
-    if box_items.count() != box_item_count:
+    box_items = list(receipt.items.select_for_update().filter(receiptitem__action=ReceiptItem.ADD, box=box).distinct())
+    if len(box_items) < box_item_count:
         raise AjaxError(RET_CONFLICT,
-                        _("Only {} items of {} available for removal.").format(box_items.count(), box_item_count))
+                        _("Only {} items of {} available for removal.").format(len(box_items), box_item_count))
 
-    ret = box.as_dict()
-    item_codes = list()
+    representative_item_id = box.representative_item_id
+    items = list()
+
+    # Firstly remove representative item, if possible.
+    box_items = sorted(box_items, key=lambda i: 0 if i.pk == representative_item_id else 1)
+
+    # Remove items until enough are removed.
     for box_item in box_items:
+        if len(items) == box_item_count:
+            break
         removal_entry = remove_item_from_receipt(request, box_item, receipt, update_receipt=False)
-        item_codes.append(removal_entry.item.code)
+        items.append({
+            "code": removal_entry.item.code,
+            "price": decimal_to_transport(removal_entry.item.price),
+        })
+
     receipt.calculate_total()
     receipt.save(update_fields=("total",))
 
+    ret = box.as_dict()
+    del ret["item_price"]  # item_price assumes representative item has same price as ones being reserved.
     ret.update(
         total=receipt.total_cents,
         changed=box_item_count,
-        item_codes=item_codes,
+        items=items,
         item_name=box.representative_item.name
     )
     return ret

kirppu/tests/test_states.py
@@ -8,7 +8,7 @@ import factory
 from .factories import *
 from .api_access import Api
 from . import ResultMixin
-from ..models import Item, Receipt
+from ..models import Item, Receipt, ReceiptItem
 
 __author__ = 'codez'
 
@@ -156,3 +156,39 @@ class StatesTest(TestCase, ResultMixin):
 
         self.assertResult(self.api.box_item_reserve(box_number=box.box_number, box_item_count=reserve_count),
                           expect=HTTPStatus.CONFLICT)
+
+    def test_box_return_receipt(self):
+        """Reserving and releasing box items should avoid representative item,
+        as it is the one used to display item price.
+        Relevant when part of box items are sold, and price of rest of its items are changed."""
+        box = BoxFactory(adopt=True, items=self.items, box_number=1)
+        Item.objects.all().update(state=Item.BROUGHT)
+
+        representative_item_id = box.representative_item_id
+        receipt = self.assertSuccess(self.api.receipt_start()).json()
+
+        def check_count(n):
+            self.assertEqual(n, Item.objects.filter(state=Item.STAGED).count())
+            self.assertEqual(n, ReceiptItem.objects.filter(receipt__pk=receipt["id"], action=ReceiptItem.ADD).count())
+
+        self.assertSuccess(self.api.box_item_reserve(box_number=1, box_item_count=4))
+        self.assertEqual(4, Item.objects.filter(state=Item.STAGED).count())
+        # Representative item should not be added to the receipt first.
+        self.assertEqual(Item.BROUGHT, Item.objects.get(pk=representative_item_id).state)
+        self.assertSuccess(self.api.box_item_release(box_number=1, box_item_count=2))
+        self.assertEqual(2, Item.objects.filter(state=Item.STAGED).count())
+
+        # Representative item should be first to be released.
+        self.assertSuccess(self.api.box_item_reserve(box_number=1, box_item_count=8))
+        check_count(10)
+        self.assertEqual(Item.STAGED, Item.objects.get(pk=representative_item_id).state)
+        self.assertSuccess(self.api.box_item_release(box_number=1, box_item_count=1))
+        check_count(9)
+        self.assertEqual(Item.BROUGHT, Item.objects.get(pk=representative_item_id).state)
+
+        self.assertSuccess(self.api.box_item_reserve(box_number=1, box_item_count=1))
+        check_count(10)
+        self.assertEqual(Item.STAGED, Item.objects.get(pk=representative_item_id).state)
+        self.assertSuccess(self.api.box_item_release(box_number=1, box_item_count=2))
+        check_count(8)
+        self.assertEqual(Item.BROUGHT, Item.objects.get(pk=representative_item_id).state)

kirppu/checkout_api.py
@@ -42,6 +42,7 @@ from .models import (
     Box,
     TemporaryAccessPermit,
     TemporaryAccessPermitLog,
+    decimal_to_transport
 )
 from .fields import ItemPriceField
 from .forms import remove_item_from_receipt
@@ -353,31 +354,70 @@ def item_search(request, event, query, code, vendor, min_price, max_price, item_
         item_dict['vendor'] = item.vendor.as_dict()
         results.append(item_dict)
 
-    box_item_counts = dict(
+    box_item_details = dict()
+    box_item_detail_query = (
         Item.objects
-            .filter(*box_clauses, box__isnull=False)
-            .values("box")
+            .filter(*box_clauses, box__isnull=False, vendor__event=event)
+            .values("box", "price", "state")
             .annotate(item_count=Count("id"))
-            .values_list("box", "item_count")
+            .values_list("box", "item_count", "price", "state")
     )
+    box_item_counts = dict()
+    for entry in box_item_detail_query:
+        box_id = entry[0]
+        count_prices = box_item_details.setdefault(box_id, list())
+        count_prices.append(
+            {
+                "count": entry[1],
+                "price": decimal_to_transport(entry[2]),
+                "state": entry[3],
+            }
+        )
+        box_item_counts[box_id] = entry[1] + box_item_counts.get(box_id, 0)
 
-    for item in (
-        Item.objects
-        .filter(*box_clauses, box__representative_item__id=F("pk"), vendor__event=event)
-        .select_related("box", "itemtype", "vendor", "vendor__user")
-        .all()
-    ):
-        item_dict = item.as_dict()
-        item_dict['name'] = item.box.description
-        item_dict['vendor'] = item.vendor.as_dict()
-        item_dict['box'] = {
-            "box_number": item.box.box_number,
-            "bundle_size": item.box.bundle_size,
-            "item_count": box_item_counts[item.box_id],
-        }
-        results.append(item_dict)
+    if box_item_counts:
+        for item in (
+            Item.objects
+            .filter(*box_clauses, box__representative_item__id=F("pk"), vendor__event=event)
+            .select_related("box", "itemtype", "vendor", "vendor__user")
+            .all()
+        ):
+            item_dict = item.as_dict()
+            details = box_item_details[item.box_id]
+            box_item_states = {g["state"] for g in details}
+            for k, v in Box.ITEM_SET_REDUCED_STATE.items():
+                if v(box_item_states):
+                    item_dict["state"] = k
+                    item_dict["state_display"] = Item(state=k).get_state_display()
+                    break
+            else:
+                item_dict["state"] = "??"
+                item_dict["state_display"] = "??: " + str(box_item_states)
+                logger.error("No matching group for states: %s", str(box_item_states))
+
+            item_dict['name'] = item.box.description
+            item_dict['vendor'] = item.vendor.as_dict()
+            item_dict['box'] = {
+                "box_number": item.box.box_number,
+                "bundle_size": item.box.bundle_size,
+                "item_count": box_item_counts[item.box_id],
+                "item_prices": details,
+            }
+            results.append(item_dict)
 
     return results
+
+
+def _box_prices(box: Box) -> list:
+    price_counts = box.item_set.values("price", "state").annotate(count=Count("id"))
+    return list(
+        {
+            "count": p["count"],
+            "price": decimal_to_transport(p["price"]),
+            "state": p["state"],
+        }
+        for p in price_counts
+    )
 
 
 @ajax_func('^item/edit$', method='POST', overseer=True, atomic=True)
@@ -392,7 +432,12 @@ def item_edit(request, event, code, price, state):
 
     item = _get_item_or_404(code, for_update=True, event=event)
     if item.box_id is not None:
-        raise AjaxError(RET_CONFLICT, "Changing box details is not implemented.")
+        return _box_edit(request, item.box, price, state)
+    else:
+        return _item_edit(request, item, price, state)
+
+
+def _item_edit(request, item, price, state):
     updates = set()
 
     if price != item.price:
@@ -440,6 +485,30 @@ def item_edit(request, event, code, price, state):
 
     item_dict = item.as_dict()
     item_dict['vendor'] = item.vendor.as_dict()
+    return item_dict
+
+
+def _box_edit(request, box, price, state):
+    if box.item_set.filter(state=Item.STAGED).exists():
+        raise AjaxError(RET_LOCKED, _("Some box items are currently staged and cannot be changed."))
+    available_items = box.item_set.filter(state__in=(Item.ADVERTISED, Item.BROUGHT))
+    any_item = available_items.first()
+    if any_item is None:
+        raise AjaxError(RET_BAD_REQUEST, _("Nothing left to change"))
+    if any(i.price != any_item.price for i in available_items):
+        raise AjaxError(RET_CONFLICT, "Available box item prices are in conflicting state")
+
+    if price != any_item.price:
+        available_items.update(price=price)
+
+    representative = box.representative_item
+    item_dict = representative.as_dict()
+    item_dict['vendor'] = representative.vendor.as_dict()
+    item_dict['box'] = {
+        "box_number": box.box_number,
+        "bundle_size": box.bundle_size,
+        "item_prices": _box_prices(box),
+    }
     return item_dict
 
 
@@ -537,20 +606,43 @@ def box_list(request, vendor):
         .select_related("representative_item", "representative_item__itemtype")
         .annotate(
             count=Count("item"),
-            brought=Count("item", Q(item__state__in=(Item.BROUGHT, Item.STAGED, Item.SOLD, Item.RETURNED))),
-            sold=Count("item", Q(item__state=Item.SOLD)),
-            compensated=Count("item", Q(item__state=Item.COMPENSATED)),
+            brought=Count("item", Q(item__state__in=(
+                Item.BROUGHT, Item.STAGED, Item.SOLD, Item.COMPENSATED, Item.RETURNED))),
             returnable=Count("item", Q(item__state__in=(Item.BROUGHT, Item.STAGED))),
         )
         .distinct()
     )
+    box_detail_data = (
+        Box.objects
+        .filter(item__vendor__id=vendor, item__hidden=False)
+        .values("id", "item__price")
+        .annotate(
+            sold=Count("item", Q(item__state=Item.SOLD)),
+            compensated=Count("item", Q(item__state=Item.COMPENSATED)),
+        )
+        .distinct()
+    )
+    box_details = {}
+    for detail in box_detail_data:
+        box_details.setdefault(detail["id"], []).append(detail)
+
     for box in boxes:
-        data = box.as_dict(exclude=("item_count",))  # item_count is already resolved more efficiently.
+        # item_count is already resolved more efficiently.
+        # item_price may be multi-valued.
+        data = box.as_dict(exclude=("item_count", "item_price"))
+        details = box_details[box.id]
         data["item_count"] = box.count
         data["items_brought_total"] = box.brought
-        data["items_sold"] = box.sold
-        data["items_compensated"] = box.compensated
         data["items_returnable"] = box.returnable
+
+        for detail in details:
+            stat = {
+                "items_sold": detail["sold"],
+                "items_compensated": detail["compensated"],
+            }
+            price = decimal_to_transport(detail["item__price"])
+            data.setdefault("counts", {})[price] = stat
+
         out_boxes.append(data)
     return out_boxes
 

kirppu/forms.py
@@ -327,7 +327,7 @@ def remove_item_from_receipt(request, item_or_code, receipt_id, update_receipt=T
         .order_by("-add_time")
 
     if len(last_added_item) == 0:
-        raise ValueError(u"Item is not added to receipt.")
+        raise ValueError("Item {} is not added to receipt.".format(item))
     assert len(last_added_item) == 1, "Receipt content conflict."
 
     last_added_item = last_added_item[0]

kirppu/models.py
@@ -527,6 +527,17 @@ class Box(models.Model):
         __extend=as_public_dict
     )
 
+    # Functions to determine common state for box item_set states.
+    # ResultState : Callable(item_set->state_set)
+    ITEM_SET_REDUCED_STATE = {
+        "AD": lambda s: s == {"AD"},
+        "BR": lambda s: s == {"BR"},
+        "ST": lambda s: "ST" in s,
+        "SO": lambda s: "SO" in s and "ST" not in s,
+        "CO": lambda s: "CO" in s and "ST" not in s and "SO" not in s and "RE" not in s,
+        "RE": lambda s: "RE" in s and "ST" not in s and "SO" not in s,
+    }
+
     @property
     def code(self):
         return self.representative_item.code

kirppu/static_src/css/checkout.css
@@ -84,6 +84,11 @@ tr.receipt_tr_clickable:hover {
     font-size: x-large;
 }
 
+/* Vendor report box list, changed prices rows. */
+tr.table-row-groupItem > td {
+    padding: 2px 5px 2px !important;
+}
+
 .alert-blink {
     /* Container class when blinking. */
     animation: alert-blink 0.3s steps(2, start) 4;

kirppu/static_src/js/checkout/countermode.coffee
@@ -216,13 +216,20 @@ class @CounterMode extends ItemCheckoutMode
         @_create_box_arg(box)
       ).then(
         (data) =>
-          @_receipt.total += data.item_price * data.changed
+          @_receipt.total += data.items.reduce(
+            (acc, cur) => acc + cur.price,
+            0
+          )
 
           if Math.abs(data.total - @_receipt.total) >= 1
             console.error("Inconsistency: " + @_receipt.total + " != " + data.total)
 
-          for _ in data.item_codes
-            @_addRow(data)
+          for item in data.items
+            box_item =
+              box_number: data.box_number
+              description: data.description
+              price: item.price
+            @_addRow(box_item)
           @notifySuccess()
 
         (jqXHR) =>
@@ -268,13 +275,20 @@ class @CounterMode extends ItemCheckoutMode
         @_create_box_arg(box)
       ).then(
         (data) =>
-          @_receipt.total -= data.item_price * data.changed
+          @_receipt.total -= data.items.reduce(
+            (acc, cur) => acc + cur.price,
+            0
+          )
 
           if Math.abs(data.total - @_receipt.total) >= 1
             console.error("Inconsistency: " + @_receipt.total + " != " + data.total)
 
-          for _ in data.item_codes
-            @_addRow(data, true)
+          for item in data.items
+            box_item =
+              box_number: data.box_number
+              description: data.description
+              price: item.price
+            @_addRow(box_item, true)
           @notifySuccess()
 
         (jqXHR) =>

kirppu/static_src/js/checkout/vendorreport.coffee
@@ -117,23 +117,25 @@ class @VendorReport extends CheckoutMode
 
     if boxes.length > 0
       sum =
-        brought: 0
         sold: 0
         compensated: 0
       count =
         brought: 0
         sold: 0
         compensated: 0
+        returnable: 0
 
       for box in boxes
-        unit_price = box.item_price
-
         count.brought += box.items_brought_total
-        sum.brought += box.items_brought_total * unit_price
-        count.sold += box.items_sold
-        sum.sold += box.items_sold * unit_price
-        count.compensated += box.items_compensated
-        sum.compensated += box.items_compensated * unit_price
+        count.returnable += box.items_returnable
+        for unit_price of box.counts
+          detail = box.counts[unit_price]
+
+          count.sold += detail.items_sold
+          sum.sold += detail.items_sold * unit_price
+
+          count.compensated += detail.items_compensated
+          sum.compensated += detail.items_compensated * unit_price
 
       rendered_table = Template.box_report_table(
         caption: gettext("Boxes")

commit 84fb88b20971bf7e870751eb0b1215d654aa295e

Fix random conflict in box item reservation.
Regression from 87629cdcd9a4fbed3a902e25a7f8880121bc5302.

The set of returned id's might not always contain the representative
item even if it returned +1 amount of rows. In that case the removal
would have not worked and the next check would have failed.

kirppu/api/boxes_api.py
@@ -111,7 +111,10 @@ def box_item_reserve(request, event, box_number, box_item_count="1"):
     # Reserve it only when it is last item to be reserved.
     representative_item_id = box.representative_item_id
     if len(candidates) == box_item_count + 1:
-        candidates = [c for c in candidates if c != representative_item_id]
+        if representative_item_id in candidates:
+            candidates = [c for c in candidates if c != representative_item_id]
+        else:
+            candidates = candidates[:-1]
 
     if len(candidates) == box_item_count:
         items = Item.objects.filter(pk__in=candidates)

kirppu/tests/test_states.py
@@ -126,12 +126,15 @@ class StatesTest(TestCase, ResultMixin):
         expected_failure = self.api.item_reserve(code=item_code)
         self.assertEqual(HTTPStatus.LOCKED, expected_failure.status_code)
 
-    def test_normal_box_receipt(self):
-        box = BoxFactory(adopt=True, items=self.items)
+    def _register_box_brought(self, box):
         box_checkin = self.assertResult(self.api.item_checkin(code=box.representative_item.code),
                                         expect=HTTPStatus.ACCEPTED).json()
         self.assertSuccess(self.api.box_checkin(code=box.representative_item.code,
                                                 box_info=box_checkin["box"]["box_number"]))
+        return box
+
+    def test_normal_box_receipt(self):
+        box = self._register_box_brought(BoxFactory(adopt=True, items=self.items))
 
         receipt = self.assertSuccess(self.api.receipt_start()).json()
         reserve_count = 3
@@ -143,14 +146,20 @@ class StatesTest(TestCase, ResultMixin):
 
         self.assertEqual(Receipt.FINISHED, finished_receipt["status"])
 
+    def test_double_box_reserve(self):
+        # Note: This tests only two subsequent requests. Simultaneous access would behave differently.
+        box = self._register_box_brought(BoxFactory(adopt=True, items=self.items))
+        receipt = self.assertSuccess(self.api.receipt_start()).json()
+        self.assertSuccess(self.api.box_item_reserve(box_number=box.box_number, box_item_count=3))
+        self.assertSuccess(self.api.box_item_reserve(box_number=box.box_number, box_item_count=3))
+        self.assertEqual(6, Item.objects.filter(box=box, state=Item.STAGED).count())
+
     def test_box_over_reserve(self):
         reserve_count = 3
 
-        box = BoxFactory(vendor=VendorFactory(event=self.event), item_count=reserve_count - 1)
-        box_checkin = self.assertResult(self.api.item_checkin(code=box.representative_item.code),
-                                        expect=HTTPStatus.ACCEPTED).json()
-        self.assertSuccess(self.api.box_checkin(code=box.representative_item.code,
-                                                box_info=box_checkin["box"]["box_number"]))
+        box = self._register_box_brought(
+            BoxFactory(vendor=VendorFactory(event=self.event), item_count=reserve_count - 1)
+        )
 
         receipt = self.assertSuccess(self.api.receipt_start()).json()
 