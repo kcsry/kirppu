kcsry/kirppu

commit d9890245e30bf7036c05f2359cc48d89b5361ba5

Update Django to 3.1 and other serverside packages.
- django.conf.urls.url is deprecated.
- request.is_ajax() is deprecated.
- ipware.ip.get_ip was re-implemented as get_client_ip.
- factory.Faker().generate was faulty way to use Faker when laziness was
not needed. Use it directly.

kirppu/views/mobile.py
@@ -13,7 +13,7 @@ from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
-from ipware.ip import get_ip
+from ipware.ip import get_client_ip
 from ratelimit.utils import is_ratelimited
 
 from ..models import Box, Event, Item, Receipt, ReceiptExtraRow, TemporaryAccessPermit, TemporaryAccessPermitLog, Vendor
@@ -102,7 +102,7 @@ class TableContents(object):
 
 
 def _ratelimit_key(group, request):
-    return get_ip(request)
+    return get_client_ip(request)
 
 
 def _login_view(request, event):
@@ -131,7 +131,7 @@ def _login_view(request, event):
                     TemporaryAccessPermitLog.objects.create(
                         permit=permit,
                         action=TemporaryAccessPermitLog.ACTION_USE if can_use else TemporaryAccessPermitLog.ACTION_TRY,
-                        address=shorten_text(get_ip(request) + "; " + request.META.get("REMOTE_HOST", ""),
+                        address=shorten_text(get_client_ip(request) + "; " + request.META.get("REMOTE_HOST", ""),
                                              TemporaryAccessPermitLog._meta.get_field("address").max_length, False),
                         peer=shorten_text(request.META.get("HTTP_USER_AGENT", ""),
                                           TemporaryAccessPermitLog._meta.get_field("peer").max_length, False)

kirppu/admin.py
@@ -2,13 +2,12 @@ import json
 
 from django.core.exceptions import PermissionDenied
 from django.conf import settings
-from django.conf.urls import url
 from django.contrib import admin, messages
 from django.contrib.admin.models import LogEntry, ADDITION, CHANGE
 from django.contrib.admin.options import get_content_type_for_model
 from django.contrib.admin.views.main import ChangeList
 from django.db import IntegrityError, models, transaction
-from django.urls import reverse, path
+from django.urls import reverse, path, re_path
 from django.utils.encoding import force_str
 from django.utils.html import escape, format_html
 from django.utils.translation import gettext_lazy as gettext, ngettext
@@ -358,8 +357,8 @@ class ClerkAdmin(admin.ModelAdmin):
     def get_urls(self):
         info = self.opts.app_label, self.opts.model_name
         return super(ClerkAdmin, self).get_urls() + [
-            url(r'^add/bulk_unbound$', self.bulk_add_unbound, name="%s_%s_bulk" % info),
-            url(r'^add/sso$', self.add_from_sso, name="%s_%s_sso" % info),
+            re_path(r'^add/bulk_unbound$', self.bulk_add_unbound, name="%s_%s_bulk" % info),
+            re_path(r'^add/sso$', self.add_from_sso, name="%s_%s_sso" % info),
         ]
 
     def add_from_sso(self, request):

kirppu/ajax_util.py
@@ -102,9 +102,6 @@ def ajax_func(original, method='POST', params=None, defaults=None, staff_overrid
 
         @functools.wraps(func)
         def wrapper(request, event_slug, **kwargs):
-            #if not request.is_ajax():
-            #    return HttpResponseBadRequest("Invalid requester")
-
             # Prevent access if checkout is not active.
             event = get_object_or_404(Event, slug=event_slug)
             if not staff_override and not event.checkout_active:

kirppu/checkout_api.py
@@ -17,7 +17,7 @@ from django.shortcuts import (
 )
 from django.utils.translation import gettext as _
 from django.utils.timezone import now
-from ipware.ip import get_ip
+from ipware.ip import get_client_ip
 
 from .api.common import (
     get_item_or_404 as _get_item_or_404,
@@ -848,7 +848,7 @@ def vendor_token_create(request, vendor_id):
         TemporaryAccessPermitLog.objects.create(
             permit=permit,
             action=TemporaryAccessPermitLog.ACTION_INVALIDATE,
-            address=get_ip(request),
+            address=get_client_ip(request),
             peer="{0}/{1}".format(clerk.user.username, clerk.pk),
         )
     old_permits.update(state=TemporaryAccessPermit.STATE_INVALIDATED)
@@ -866,7 +866,7 @@ def vendor_token_create(request, vendor_id):
             TemporaryAccessPermitLog.objects.create(
                 permit=permit,
                 action=TemporaryAccessPermitLog.ACTION_ADD,
-                address=get_ip(request),
+                address=get_client_ip(request),
                 peer="{0}/{1}".format(clerk.user.username, clerk.pk),
             )
             break

kirppu/tests/test_states.py
@@ -3,7 +3,7 @@
 from http import HTTPStatus
 
 from django.test import Client, TestCase
-import factory
+import faker
 
 from .factories import *
 from .api_access import Api
@@ -27,7 +27,7 @@ class PublicTest(TestCase, ResultMixin):
 
     def test_register_item(self):
         data = dict(
-            name=factory.Faker("sentence", nb_words=3).generate({}),
+            name=faker.Faker().sentence(nb_words=3),
             price="1.25",
             tag_type="short",
             suffixes="",
@@ -42,7 +42,7 @@ class PublicTest(TestCase, ResultMixin):
 
     def test_register_box(self):
         data = dict(
-            description=factory.Faker("sentence", nb_words=3).generate({}),
+            description=faker.Faker().sentence(nb_words=3),
             price="1.25",
             item_type=self.type.id,
             adult=False,
@@ -55,7 +55,7 @@ class PublicTest(TestCase, ResultMixin):
 
     def test_register_box_with_single_item(self):
         data = dict(
-            description=factory.Faker("sentence", nb_words=3).generate({}),
+            description=faker.Faker().sentence(nb_words=3),
             price="1.25",
             item_type=self.type.id,
             adult=False,
@@ -68,7 +68,7 @@ class PublicTest(TestCase, ResultMixin):
 
     def test_register_single_bundle_box(self):
         data = dict(
-            description=factory.Faker("sentence", nb_words=3).generate({}),
+            description=faker.Faker().sentence(nb_words=3),
             price="1.25",
             item_type=self.type.id,
             adult=False,

kirppu/views/monolithic.py
@@ -1052,7 +1052,7 @@ def lost_and_found_list(request, event_slug):
 
 
 def kirppu_csrf_failure(request, reason=""):
-    if request.is_ajax() or request.META.get("HTTP_ACCEPT", "") == "text/json":
+    if request.META.get("HTTP_ACCEPT", "") in ("text/json", "application/json"):
         # TODO: Unify the response to match requested content type.
         return HttpResponseForbidden(
             _("CSRF verification failed. Request aborted."),

kirppu_project/settings.py
@@ -176,6 +176,7 @@ TEMPLATES = [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.i18n',
                 'django.template.context_processors.media',
+                'django.template.context_processors.request',
                 'django.template.context_processors.static',
                 'django.template.context_processors.tz',
                 'django.contrib.messages.context_processors.messages',

kompassi_oauth2/urls.py
@@ -1,10 +1,10 @@
-from django.conf.urls import url
+from django.conf.urls import re_path
 
 from .views import LoginView, CallbackView, LogoutView
 
 
 urlpatterns = [
-    url(r'^oauth2/login/?$', LoginView.as_view(), name='oauth2_login_view'),
-    url(r'^oauth2/callback/?$', CallbackView.as_view(), name='oauth2_callback_view'),
-    url(r'^oauth2/logout/?$', LogoutView.as_view(), name='oauth2_logout_view'),
+    re_path(r'^oauth2/login/?$', LoginView.as_view(), name='oauth2_login_view'),
+    re_path(r'^oauth2/callback/?$', CallbackView.as_view(), name='oauth2_callback_view'),
+    re_path(r'^oauth2/logout/?$', LogoutView.as_view(), name='oauth2_logout_view'),
 ]

commit 11856df0b888377c34bdb9c27e2d09c18d1b11d2

Fix mobile code ratelimit.
Regression from d9890245e30bf7036c05f2359cc48d89b5361ba5.

kirppu/views/mobile.py
@@ -101,8 +101,13 @@ class TableContents(object):
         self.sum = Decimal(0)
 
 
+def _get_client_ip(request):
+    client_ip, routable = get_client_ip(request)
+    return client_ip
+
+
 def _ratelimit_key(group, request):
-    return get_client_ip(request)
+    return _get_client_ip(request)
 
 
 def _login_view(request, event):
@@ -131,7 +136,7 @@ def _login_view(request, event):
                     TemporaryAccessPermitLog.objects.create(
                         permit=permit,
                         action=TemporaryAccessPermitLog.ACTION_USE if can_use else TemporaryAccessPermitLog.ACTION_TRY,
-                        address=shorten_text(get_client_ip(request) + "; " + request.META.get("REMOTE_HOST", ""),
+                        address=shorten_text(_get_client_ip(request) + "; " + request.META.get("REMOTE_HOST", ""),
                                              TemporaryAccessPermitLog._meta.get_field("address").max_length, False),
                         peer=shorten_text(request.META.get("HTTP_USER_AGENT", ""),
                                           TemporaryAccessPermitLog._meta.get_field("peer").max_length, False)