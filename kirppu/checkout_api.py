kcsry/kirppu

commit 5d80f907eadb2c3a1316a1173058b6ac1010bf1e

Implement Event-specific permissions.
Old permissions are not migrated (nor deleted) as they are too vague.

kirppu/checkout_api.py
@@ -36,6 +36,7 @@ from .models import (
     Clerk,
     Counter,
     Event,
+    EventPermission,
     ReceiptItem,
     ReceiptExtraRow,
     Vendor,
@@ -213,8 +214,10 @@ def clerk_login(request, event, code, counter):
         raise AjaxError(RET_AUTH_FAILED, _(u"No such clerk."))
 
     clerk_data = clerk.as_dict()
-    clerk_data['overseer_enabled'] = clerk.user.has_perm('kirppu.oversee')
-    clerk_data['stats_enabled'] = clerk.user.is_staff or clerk.user.has_perm('kirppu.oversee')
+    permissions = EventPermission.get(event, request.user)
+    oversee = permissions.can_perform_overseer_actions
+    clerk_data['overseer_enabled'] = oversee
+    clerk_data['stats_enabled'] = oversee or permissions.can_see_statistics
 
     active_receipts = Receipt.objects.filter(clerk=clerk, status=Receipt.PENDING, type=Receipt.TYPE_PURCHASE)
     if active_receipts:

kirppu/accounting.py
@@ -2,14 +2,15 @@
 from collections import defaultdict
 import csv
 
-from django.contrib.auth.decorators import login_required, permission_required
+from django.contrib.auth.decorators import login_required
+from django.core.exceptions import PermissionDenied
 from django.db.models import Sum
 from django.shortcuts import get_object_or_404
 from django.utils.translation import ugettext_lazy as _, pgettext_lazy, gettext
 from django.utils import timezone
 
 from .csv_utils import csv_streamer_view, strip_generator
-from .models import Event, Item, Receipt, ReceiptExtraRow, ReceiptItem, decimal_to_transport
+from .models import Event, EventPermission, Item, Receipt, ReceiptExtraRow, ReceiptItem, decimal_to_transport
 
 COLUMNS = (
     _("Event number"),
@@ -36,9 +37,10 @@ def _zero_fn():
 
 
 @login_required
-@permission_required("kirppu.view_accounting")
 def accounting_receipt_view(request, event_slug):
     event = get_object_or_404(Event, slug=event_slug)
+    if not EventPermission.get(event, request.user).can_see_accounting:
+        raise PermissionDenied
 
     return csv_streamer_view(
         request,

kirppu/admin.py
@@ -25,6 +25,7 @@ from .forms import (
 from .models import (
     Clerk,
     Event,
+    EventPermission,
     Item,
     ItemType,
     Vendor,
@@ -126,6 +127,21 @@ Admin UI list column that displays user name with link to the user model itself.
 _user_link = RefLinkAccessor("user", ugettext(u"User"))
 
 _person_link = RefLinkAccessor("person", ugettext(u"Person"))
+
+_event_link = RefLinkAccessor("event", ugettext("Event"))
+
+
+@admin.register(EventPermission)
+class EventPermissionAdmin(admin.ModelAdmin):
+    list_display = ("id", _event_link, _user_link, "combination")
+    list_display_links = ("id", "combination")
+    list_filter = (
+        "event",
+    )
+    list_select_related = (
+        "event",
+        "user",
+    )
 
 
 @admin.register(Vendor)

kirppu/ajax_util.py
@@ -16,6 +16,7 @@ from .models import (
     Clerk,
     Counter,
     Event,
+    EventPermission,
 )
 
 """
@@ -204,7 +205,7 @@ def require_user_features(counter=True, clerk=True, overseer=False, staff_overri
                 # Thus call raises if clerk is not found.
                 clerk_obj = get_clerk(request)
 
-                if overseer and not clerk_obj.user.has_perm('kirppu.oversee'):
+                if overseer and not EventPermission.get(clerk_obj.event, clerk_obj.user).can_perform_overseer_actions:
                     raise AjaxError(RET_FORBIDDEN, _(u"Access denied."))
 
             return func(request, *args, **kwargs)

kirppu/item_dump.py
@@ -2,14 +2,15 @@
 import csv
 import typing
 
-from django.contrib.auth.decorators import login_required, permission_required
+from django.contrib.auth.decorators import login_required
+from django.core.exceptions import PermissionDenied
 from django.db.models import Max, TextField
 from django.db.models.functions import Length, Cast
 from django.shortcuts import get_object_or_404
 from django.utils.translation import ugettext_lazy as _, gettext
 
 from .csv_utils import csv_streamer_view
-from .models import Event, Item
+from .models import Event, EventPermission, Item
 
 
 COLUMNS = (
@@ -24,9 +25,10 @@ COLUMNS = (
 
 
 @login_required
-@permission_required("kirppu.view_accounting")
 def dump_items_view(request, event_slug):
     event = get_object_or_404(Event, slug=event_slug)
+    if not EventPermission.get(event, request.user).can_see_accounting:
+        raise PermissionDenied
 
     as_text = request.GET.get("txt") is not None
 

kirppu/migrations/0032_eventpermission.py
@@ -1,0 +1,32 @@
+from django.conf import settings
+from django.db import migrations, models
+import django.db.models.deletion
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+        ('kirppu', '0031_event_age_restricted_items'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='EventPermission',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('can_see_clerk_codes', models.BooleanField(default=False)),
+                ('can_see_statistics', models.BooleanField(default=False)),
+                ('can_see_accounting', models.BooleanField(default=False)),
+                ('can_register_items_outside_registration', models.BooleanField(default=False)),
+                ('can_perform_overseer_actions', models.BooleanField(default=False)),
+                ('can_switch_sub_vendor', models.BooleanField(default=False)),
+                ('can_create_sub_vendor', models.BooleanField(default=False)),
+                ('event', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='kirppu.Event')),
+                ('user', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
+            ],
+            options={
+                'unique_together': {('event', 'user')},
+            },
+        ),
+    ]

kirppu/models.py
@@ -151,6 +151,61 @@ class Event(models.Model):
         return reverse("kirppu:vendor_view", kwargs={"event_slug": self.slug})
 
 
+class EventPermission(models.Model):
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    can_see_clerk_codes = models.BooleanField(default=False)
+    can_see_statistics = models.BooleanField(default=False)
+    can_see_accounting = models.BooleanField(default=False)
+    can_register_items_outside_registration = models.BooleanField(default=False)
+    can_perform_overseer_actions = models.BooleanField(default=False)
+    can_switch_sub_vendor = models.BooleanField(default=False)
+    can_create_sub_vendor = models.BooleanField(default=False)
+
+    _short_mapping = (
+        (can_see_clerk_codes, "CCodes"),
+        (can_see_statistics, "STAT"),
+        (can_see_accounting, "ACC"),
+        (can_register_items_outside_registration, "REG"),
+        (can_perform_overseer_actions, "OVER"),
+        (can_switch_sub_vendor, "SubSw"),
+        (can_create_sub_vendor, "SubCr"),
+    )
+
+    class Meta:
+        unique_together = (("event", "user"),)
+
+    def __str__(self):
+        return "{} / {}".format(self.event, self.user)
+
+    @short_description(_("Permissions"))
+    def combination(self):
+        enabled_names = list(
+            value
+            for key, value in self._short_mapping
+            if getattr(self, key.name)
+        )
+        if not enabled_names:
+            return "----"
+        return ", ".join(enabled_names)
+
+    @classmethod
+    def get(cls, event, user):
+        try:
+            return cls.objects.get(event=event, user=user)
+        except cls.DoesNotExist:
+            fields = cls._meta.get_fields()
+            args = {
+                'event': event,
+                'user': user,
+            }
+            for field in fields:
+                if not field.is_relation and field.default != models.fields.NOT_PROVIDED:
+                    args[field.name] = field.default
+            return cls(**args)
+
+
 @python_2_unicode_compatible
 class Clerk(models.Model):
     event = models.ForeignKey(Event, on_delete=models.CASCADE)

kirppu/utils.py
@@ -166,11 +166,13 @@ def is_vendor_open(request, event):
     Test if Item edit for vendor is currently open.
 
     :param request: Optional Django request for checking whether current user has override permission for registration.
+    :type event: Event
     :return: True if open, False if not and modifications by vendor must not be allowed.
     """
     end = event.registration_end
+    from .models import EventPermission
     return (end is not None and timezone.now() <= end) or (
-        request.user.has_perm("kirppu.register_override")
+        EventPermission.get(event, request.user).can_add_items_outside_registration
     )
 
 
@@ -196,24 +198,6 @@ def require_vendor_open(fn):
             return HttpResponseForbidden(_(u"Registration is closed"))
         return fn(request, event, *args, **kwargs)
     return inner
-
-
-def require_test(test):
-    """
-    Decorate view function so that it will return Forbidden if given test does not return True when called.
-
-    :param test: Test function. It will be called with `request` argument.
-    :type test: callable
-    :return: Decorated function.
-    """
-    def wrapper(fn):
-        @wraps(fn)
-        def inner(request, *args, **kwargs):
-            if not test(request):
-                raise PermissionDenied()
-            return fn(request, *args, **kwargs)
-        return inner
-    return wrapper
 
 
 def barcode_view(fn):

commit eb3cad2acb4c78548a95b133ddcbfb315693c96c

Fix missing overseer link.
Regression from 5d80f907eadb2c3a1316a1173058b6ac1010bf1e.

kirppu/checkout_api.py
@@ -147,7 +147,7 @@ def clerk_login(request, event, code, counter):
         raise AjaxError(RET_AUTH_FAILED, _(u"No such clerk."))
 
     clerk_data = clerk.as_dict()
-    permissions = EventPermission.get(event, request.user)
+    permissions = EventPermission.get(event, clerk.user)
     oversee = permissions.can_perform_overseer_actions
     clerk_data['overseer_enabled'] = oversee
     clerk_data['stats_enabled'] = oversee or permissions.can_see_statistics