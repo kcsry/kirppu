kcsry/kirppu

commit 3c8ea31656a44c077e8e24faed9ecbe210d91b90

Fix concurrency safety on various objects.
Optimize object updates.

kirppu/forms.py
@@ -305,14 +305,7 @@ class ReceiptAdminForm(forms.ModelForm):
 
 class ItemRemoveForm(forms.Form):
     receipt = forms.IntegerField(min_value=0, label=u"Receipt ID")
-    item = forms.CharField(label=u"Item code")
-
-    def __init__(self, *args, **kwargs):
-        super(ItemRemoveForm, self).__init__(*args, **kwargs)
-        self.last_added_item = None  # type: ReceiptItem
-        self.item = None  # type: Item
-        self.receipt = None  # type: Receipt
-        self.removal_entry = None  # type: ReceiptItem
+    code = forms.CharField(label=u"Item code")
 
     def clean_receipt(self):
         data = self.cleaned_data["receipt"]
@@ -321,53 +314,51 @@ class ItemRemoveForm(forms.Form):
         return data
 
     def clean_item(self):
-        data = self.cleaned_data["item"]
+        data = self.cleaned_data["code"]
         if not Item.objects.filter(code=data).exists():
             raise forms.ValidationError(u"Item with code {code} not found.".format(code=data))
         return data
 
-    def clean(self):
-        cleaned_data = super(ItemRemoveForm, self).clean()
-        if "receipt" not in cleaned_data or "item" not in cleaned_data:
-            return cleaned_data
-        receipt_id = cleaned_data["receipt"]
-        code = cleaned_data["item"]
 
-        item = Item.objects.get(code=code)
-        receipt = Receipt.objects.get(pk=receipt_id, type=Receipt.TYPE_PURCHASE)
+@transaction.atomic
+def remove_item_from_receipt(request, item_or_code, receipt_id, update_receipt=True):
+    if isinstance(item_or_code, Item):
+        item = item_or_code
+    else:
+        item = Item.objects.select_for_update().get(code=item_or_code)
 
-        last_added_item = ReceiptItem.objects\
-            .filter(receipt=receipt, item=item, action=ReceiptItem.ADD)\
-            .order_by("-add_time")
+    if isinstance(receipt_id, Receipt):
+        receipt = receipt_id
+        assert receipt.type == Receipt.TYPE_PURCHASE, "This function cannot be used for non-purchase receipts."
+    else:
+        receipt = Receipt.objects.select_for_update().get(pk=receipt_id, type=Receipt.TYPE_PURCHASE)
+        assert update_receipt, "Receipt must be updated if accessed by id."
 
-        if len(last_added_item) == 0:
-            raise forms.ValidationError(u"Item is not added to receipt.")
-        assert len(last_added_item) == 1
+    last_added_item = ReceiptItem.objects \
+        .filter(receipt=receipt, item=item, action=ReceiptItem.ADD) \
+        .select_for_update() \
+        .order_by("-add_time")
 
-        self.last_added_item = last_added_item
-        self.item = item
-        self.receipt = receipt
-        return cleaned_data
+    if len(last_added_item) == 0:
+        raise ValueError(u"Item is not added to receipt.")
+    assert len(last_added_item) == 1, "Receipt content conflict."
 
-    @transaction.atomic
-    def save(self, request):
-        assert self.last_added_item is not None
+    last_added_item = last_added_item[0]
+    last_added_item.action = ReceiptItem.REMOVED_LATER
+    last_added_item.save(update_fields=("action",))
 
-        last_added_item = self.last_added_item[0]
-        last_added_item.action = ReceiptItem.REMOVED_LATER
-        last_added_item.save()
+    removal_entry = ReceiptItem(item=item, receipt=receipt, action=ReceiptItem.REMOVE)
+    removal_entry.save()
 
-        removal_entry = ReceiptItem(item=self.item, receipt=self.receipt, action=ReceiptItem.REMOVE)
-        removal_entry.save()
+    if update_receipt:
+        receipt.calculate_total()
+        receipt.save(update_fields=("total",))
 
-        self.receipt.calculate_total()
-        self.receipt.save()
-
-        if self.item.state != Item.BROUGHT:
-            ItemStateLog.objects.log_state(item=self.item, new_state=Item.BROUGHT, request=request)
-            self.item.state = Item.BROUGHT
-            self.item.save()
-        self.removal_entry = removal_entry
+    if item.state != Item.BROUGHT:
+        ItemStateLog.objects.log_state(item=item, new_state=Item.BROUGHT, request=request)
+        item.state = Item.BROUGHT
+        item.save(update_fields=("state",))
+    return removal_entry
 
 
 class VendorSetSelfForm(forms.ModelForm):

kirppu/api/boxes_api.py
@@ -1,6 +1,4 @@
 # -*- coding: utf-8 -*-
-from itertools import chain
-
 from django.utils.translation import ugettext as _
 
 from .common import (
@@ -11,7 +9,7 @@ from .common import (
 )
 from ..ajax_util import AjaxError, RET_BAD_REQUEST, RET_CONFLICT
 from ..checkout_api import ajax_func
-from ..forms import ItemRemoveForm
+from ..forms import remove_item_from_receipt
 from ..models import Item, ItemStateLog, ReceiptItem
 
 __author__ = 'codez'
@@ -86,11 +84,12 @@ def box_item_reserve(request, box_number, box_item_count="1"):
     box = _get_box_or_404(box_number)
 
     receipt_id = request.session["receipt"]
-    receipt = get_receipt(receipt_id)
+    receipt = get_receipt(receipt_id, for_update=True)
 
     # Must force id-list to ensure stability.
     # Otherwise the "list" is considered as a subquery which may not be stable.
-    candidates = list(box.item_set.filter(state=Item.BROUGHT)[:box_item_count].values_list("pk", flat=True))
+    candidates = list(box.item_set.select_for_update()
+                      .filter(state=Item.BROUGHT)[:box_item_count].values_list("pk", flat=True))
     if len(candidates) == box_item_count:
         items = Item.objects.filter(pk__in=candidates)
         rows = [
@@ -106,7 +105,7 @@ def box_item_reserve(request, box_number, box_item_count="1"):
 
         ReceiptItem.objects.bulk_create(rows)
         receipt.calculate_total()
-        receipt.save()
+        receipt.save(update_fields=("total",))
 
         ret = box.as_dict()
         ret.update(
@@ -127,30 +126,20 @@ def box_item_release(request, box_number, box_item_count="1"):
     box = _get_box_or_404(box_number)
 
     receipt_id = request.session["receipt"]
-    receipt = get_receipt(receipt_id)
+    receipt = get_receipt(receipt_id, for_update=True)
 
-    box_items = receipt.items.filter(receiptitem__action=ReceiptItem.ADD, box=box)[:box_item_count]
+    box_items = receipt.items.select_for_update().filter(receiptitem__action=ReceiptItem.ADD, box=box)[:box_item_count]
     if box_items.count() != box_item_count:
         raise AjaxError(RET_CONFLICT,
                         _("Only {} items of {} available for removal.").format(box_items.count(), box_item_count))
 
-    forms = [
-        ItemRemoveForm({
-            'receipt': receipt_id,
-            'item': item.code,
-        })
-        for item in box_items
-    ]
-    if not all(form.is_valid() for form in forms):
-        raise AjaxError(RET_CONFLICT, ", ".join(chain(form.errors for form in forms)))
-
     ret = box.as_dict()
     item_codes = list()
-    for form in forms:
-        form.save(request)
-        item_codes.append(form.removal_entry.item.code)
+    for box_item in box_items:
+        removal_entry = remove_item_from_receipt(request, box_item, receipt, update_receipt=False)
+        item_codes.append(removal_entry.item.code)
     receipt.calculate_total()
-    receipt.save()
+    receipt.save(update_fields=("total",))
 
     ret.update(
         total=receipt.total_cents,

kirppu/api/common.py
@@ -8,16 +8,20 @@ from ..models import Box, Item, Receipt
 __author__ = 'codez'
 
 
-def get_item_or_404(code, **kwargs):
+def get_item_or_404(code, for_update=False, **kwargs):
     """
     :param code: Item barcode to find.
     :type code: str
+    :param for_update: If True, Item is retrieved for update.
     :param kwargs: Extra query filters.
     :rtype: Item
     :raises Http404: If an Item matching the query does not exist.
     """
     try:
-        item = Item.get_item_by_barcode(code, **kwargs)
+        if for_update:
+            item = Item.get_item_by_barcode_for_update(code, **kwargs)
+        else:
+            item = Item.get_item_by_barcode(code, **kwargs)
     except Item.DoesNotExist:
         item = None
 
@@ -26,10 +30,11 @@ def get_item_or_404(code, **kwargs):
     return item
 
 
-def get_box_or_404(box_number, **kwargs):
+def get_box_or_404(box_number, for_update=False, **kwargs):
     """
     :param box_number: Number of the box to find.
     :type box_number: int|str
+    :param for_update: If True, Box is retrieved for update.
     :param kwargs: Extra query filters.
     :rtype: Box
     :raises Http404: If a Box matching the query does not exist.
@@ -38,7 +43,10 @@ def get_box_or_404(box_number, **kwargs):
     number = None
     try:
         number = int(box_number)
-        box = Box.objects.get(box_number=box_number, **kwargs)
+        query = Box.objects
+        if for_update:
+            query = query.select_for_update()
+        box = query.get(box_number=box_number, **kwargs)
         return box
     except ValueError as e:
         raise AjaxError(RET_BAD_REQUEST, "box_number must be a number")
@@ -56,8 +64,11 @@ def item_state_conflict(item):
     )
 
 
-def get_receipt(receipt_id):
+def get_receipt(receipt_id, for_update=False):
     try:
-        return Receipt.objects.get(pk=receipt_id, type=Receipt.TYPE_PURCHASE)
+        query = Receipt.objects
+        if for_update:
+            query = query.select_for_update()
+        return query.get(pk=receipt_id, type=Receipt.TYPE_PURCHASE)
     except Receipt.DoesNotExist:
         raise AjaxError(500, "Receipt {} does not exist.".format(receipt_id))

kirppu/checkout_api.py
@@ -46,7 +46,7 @@ from .models import (
     TemporaryAccessPermitLog,
 )
 from .fields import ItemPriceField
-from .forms import ItemRemoveForm
+from .forms import remove_item_from_receipt
 
 from . import ajax_util, stats
 from .ajax_util import (
@@ -163,7 +163,10 @@ def checkout_js(request):
 
 @transaction.atomic
 def item_mode_change(request, code, from_, to, message_if_not_first=None):
-    item = _get_item_or_404(code)
+    if isinstance(code, Item):
+        item = code
+    else:
+        item = _get_item_or_404(code, for_update=True)
     if not isinstance(from_, tuple):
         from_ = (from_,)
     if item.state in from_:
@@ -175,7 +178,7 @@ def item_mode_change(request, code, from_, to, message_if_not_first=None):
         ItemStateLog.objects.log_state(item=item, new_state=to, request=request)
         old_state = item.state
         item.state = to
-        item.save()
+        item.save(update_fields=("state", "hidden"))
         ret = item.as_dict()
         if message_if_not_first is not None and len(from_) > 1 and old_state != from_[0]:
             ret.update(_message=message_if_not_first)
@@ -339,9 +342,11 @@ def item_edit(request, code, price, state):
     if state not in {st for (st, _) in Item.STATE}:
         raise AjaxError(RET_BAD_REQUEST, 'Unknown state: {0}'.format(state))
 
-    item = _get_item_or_404(code)
+    item = _get_item_or_404(code, for_update=True)
+    updates = set()
 
     if price != item.price:
+        updates.add("price")
         price_editable_states = {
             Item.ADVERTISED,
             Item.BROUGHT,
@@ -354,6 +359,7 @@ def item_edit(request, code, price, state):
             )
 
     if item.state != state:
+        updates.add("state")
         unsold_states = {
             Item.ADVERTISED,
             Item.BROUGHT,
@@ -369,12 +375,7 @@ def item_edit(request, code, price, state):
             ).values_list('receipt_id', flat=True)
 
             for receipt_id in receipt_ids:
-                remove_form = ItemRemoveForm({
-                    'receipt': receipt_id,
-                    'item': item.code,
-                })
-                assert remove_form.is_valid()
-                remove_form.save(request)
+                remove_item_from_receipt(request, item, receipt_id)
         else:
             raise AjaxError(
                 RET_BAD_REQUEST,
@@ -385,7 +386,7 @@ def item_edit(request, code, price, state):
 
     item.state = state
     item.price = price
-    item.save()
+    item.save(update_fields=updates)
 
     item_dict = item.as_dict()
     item_dict['vendor'] = item.vendor.as_dict()
@@ -490,7 +491,7 @@ def box_list(request, vendor):
 
 @ajax_func('^item/checkin$', atomic=True)
 def item_checkin(request, code):
-    item = _get_item_or_404(code)
+    item = _get_item_or_404(code, for_update=True)
     if not item.vendor.terms_accepted:
         raise AjaxError(500, _(u"Vendor has not accepted terms!"))
 
@@ -510,12 +511,12 @@ def item_checkin(request, code):
         # TODO: Consider returning bad request to clearly separate actual success.
         return JsonResponse(response, status=RET_ACCEPTED, reason="OTHER API")
 
-    return item_mode_change(request, code, Item.ADVERTISED, Item.BROUGHT)
+    return item_mode_change(request, item, Item.ADVERTISED, Item.BROUGHT)
 
 
 @ajax_func('^item/checkout$', atomic=True)
 def item_checkout(request, code, vendor=None):
-    item = _get_item_or_404(code)
+    item = _get_item_or_404(code, for_update=True)
     if vendor == "":
         vendor = None
     if vendor is not None:
@@ -556,7 +557,7 @@ def item_checkout(request, code, vendor=None):
         }
         return ret
     else:
-        return item_mode_change(request, code, (Item.BROUGHT, Item.ADVERTISED), Item.RETURNED,
+        return item_mode_change(request, item, (Item.BROUGHT, Item.ADVERTISED), Item.RETURNED,
                                 _(u"Item was not brought to event."))
 
 
@@ -588,14 +589,14 @@ def item_compensate(request, code):
     if "compensation" not in request.session:
         raise AjaxError(RET_CONFLICT, _(u"No compensation started!"))
     receipt_pk, vendor_id = request.session["compensation"]
-    receipt = Receipt.objects.get(pk=receipt_pk, type=Receipt.TYPE_COMPENSATION)
+    receipt = Receipt.objects.select_for_update().get(pk=receipt_pk, type=Receipt.TYPE_COMPENSATION)
 
-    item = _get_item_or_404(code, vendor=vendor_id)
-    item_dict = item_mode_change(request, code, Item.SOLD, Item.COMPENSATED)
+    item = _get_item_or_404(code, vendor=vendor_id, for_update=True)
+    item_dict = item_mode_change(request, item, Item.SOLD, Item.COMPENSATED)
 
     ReceiptItem.objects.create(item=item, receipt=receipt)
     receipt.calculate_total()
-    receipt.save()
+    receipt.save(update_fields=("total",))
 
     return item_dict
 
@@ -606,7 +607,7 @@ def item_compensate_end(request):
         raise AjaxError(RET_CONFLICT, _(u"No compensation started!"))
 
     receipt_pk, vendor_id = request.session["compensation"]
-    receipt = Receipt.objects.get(pk=receipt_pk, type=Receipt.TYPE_COMPENSATION)
+    receipt = Receipt.objects.select_for_update().get(pk=receipt_pk, type=Receipt.TYPE_COMPENSATION)
 
     provision = Provision(vendor_id=vendor_id, receipt=receipt)
     if provision.has_provision:
@@ -626,7 +627,7 @@ def item_compensate_end(request):
     receipt.status = Receipt.FINISHED
     receipt.end_time = now()
     receipt.calculate_total()
-    receipt.save()
+    receipt.save(update_fields=("status", "end_time", "total"))
 
     del request.session["compensation"]
 
@@ -747,20 +748,20 @@ def receipt_start(request):
 
 @ajax_func('^item/reserve$', atomic=True)
 def item_reserve(request, code):
-    item = _get_item_or_404(code)
+    item = _get_item_or_404(code, for_update=True)
     receipt_id = request.session["receipt"]
-    receipt = receipt = get_receipt(receipt_id)
+    receipt = get_receipt(receipt_id, for_update=True)
 
     message = raise_if_item_not_available(item)
     if item.state in (Item.ADVERTISED, Item.BROUGHT, Item.MISSING):
         ItemStateLog.objects.log_state(item, Item.STAGED, request=request)
         item.state = Item.STAGED
-        item.save()
+        item.save(update_fields=("state",))
 
         ReceiptItem.objects.create(item=item, receipt=receipt)
         # receipt.items.create(item=item)
         receipt.calculate_total()
-        receipt.save()
+        receipt.save(update_fields=("total",))
 
         ret = item.as_dict()
         ret.update(total=receipt.total_cents)
@@ -774,17 +775,14 @@ def item_reserve(request, code):
 
 @ajax_func('^item/release$', atomic=True)
 def item_release(request, code):
-    item = _get_item_or_404(code)
+    item = _get_item_or_404(code, for_update=True)
     receipt_id = request.session["receipt"]
-    remove_form = ItemRemoveForm({
-        'receipt': receipt_id,
-        'item': code,
-    })
-    if not remove_form.is_valid():
-        raise AjaxError(RET_CONFLICT, ", ".join(remove_form.errors))
+    try:
+        removal_entry = remove_item_from_receipt(request, item, receipt_id)
+    except ValueError as e:
+        raise AjaxError(RET_CONFLICT, e.args[0])
 
-    remove_form.save(request)
-    return remove_form.removal_entry.as_dict()
+    return removal_entry.as_dict()
 
 
 def _get_active_receipt(request, id, allowed_states=(Receipt.PENDING,)):
@@ -800,7 +798,7 @@ def _get_active_receipt(request, id, allowed_states=(Receipt.PENDING,)):
         receipt_id = arg_id
         logger.warning("Active receipt is being read without it being in session: %i", receipt_id)
 
-    receipt = receipt = get_receipt(receipt_id)
+    receipt = get_receipt(receipt_id, for_update=True)
     if receipt.status not in allowed_states:
         if not in_session and receipt.status == Receipt.FINISHED:
             raise AjaxError(RET_CONFLICT, "Receipt {} was already ended at {}".format(receipt_id, receipt.end_time))
@@ -815,11 +813,10 @@ def receipt_finish(request, id):
 
     receipt.end_time = now()
     receipt.status = Receipt.FINISHED
-    receipt.save()
+    receipt.save(update_fields=("end_time", "status"))
 
-    receipt_items = Item.objects.filter(receipt=receipt, receiptitem__action=ReceiptItem.ADD)
-    for item in receipt_items:
-        ItemStateLog.objects.log_state(item=item, new_state=Item.SOLD, request=request)
+    receipt_items = Item.objects.select_for_update().filter(receipt=receipt, receiptitem__action=ReceiptItem.ADD)
+    ItemStateLog.objects.log_states(item_set=receipt_items, new_state=Item.SOLD, request=request)
     receipt_items.update(state=Item.SOLD)
 
     del request.session["receipt"]
@@ -831,7 +828,7 @@ def receipt_abort(request, id):
     receipt, receipt_id = _get_active_receipt(request, id, (Receipt.PENDING, Receipt.SUSPENDED))
 
     # For all ADDed items, add REMOVE-entries and return the real Item's back to available.
-    added_items = ReceiptItem.objects.filter(receipt_id=receipt_id, action=ReceiptItem.ADD)
+    added_items = ReceiptItem.objects.select_for_update().filter(receipt_id=receipt_id, action=ReceiptItem.ADD)
     for receipt_item in added_items.only("item"):
         item = receipt_item.item
 
@@ -852,7 +849,7 @@ def receipt_abort(request, id):
     receipt.end_time = now()
     receipt.status = Receipt.ABORTED
     receipt.calculate_total()
-    receipt.save()
+    receipt.save(update_fields=("end_time", "status", "total"))
 
     del request.session["receipt"]
     return receipt.as_dict()
@@ -958,7 +955,7 @@ def items_abandon(request, vendor):
 
 @ajax_func('^item/mark_lost$', overseer=True, atomic=True)
 def item_mark_lost(request, code):
-    item = get_object_or_404(Item, code=code)
+    item = _get_item_or_404(code=code, for_update=True)
     if item.state == Item.SOLD:
         raise AjaxError(RET_CONFLICT, u"Item is sold!")
     if item.state == Item.STAGED:
@@ -967,7 +964,7 @@ def item_mark_lost(request, code):
         raise AjaxError(RET_CONFLICT, u"Item is abandoned.")
 
     item.lost_property = True
-    item.save()
+    item.save(update_fields=("lost_property",))
     return item.as_dict()
 
 

kirppu/models.py
@@ -792,6 +792,10 @@ class Item(models.Model):
         """
         return Item.objects.get(code=data, **kwargs)
 
+    @staticmethod
+    def get_item_by_barcode_for_update(data, **kwargs):
+        return Item.objects.select_for_update().get(code=data, **kwargs)
+
     @classmethod
     def is_item_barcode(cls, text):
         """

kirppu/views.py
@@ -35,7 +35,7 @@ from django.views.generic import RedirectView
 
 from .checkout_api import clerk_logout_fn
 from . import ajax_util
-from .forms import ItemRemoveForm, VendorItemForm, VendorBoxForm
+from .forms import ItemRemoveForm, VendorItemForm, VendorBoxForm, remove_item_from_receipt as _remove_item_from_receipt
 from .fields import ItemPriceField
 from .models import (
     Box,
@@ -112,10 +112,10 @@ def item_add(request):
 @require_http_methods(["POST"])
 def item_hide(request, code):
     vendor = Vendor.get_vendor(request)
-    item = get_object_or_404(Item.objects, code=code, vendor=vendor)
+    item = get_object_or_404(Item.objects.select_for_update(), code=code, vendor=vendor)
 
     item.hidden = True
-    item.save()
+    item.save(update_fields=("hidden",))
 
     return HttpResponse()
 
@@ -124,7 +124,7 @@ def item_hide(request, code):
 @require_http_methods(['POST'])
 def item_to_not_printed(request, code):
     vendor = Vendor.get_vendor(request)
-    item = get_object_or_404(Item.objects, code=code, vendor=vendor, box__isnull=True)
+    item = get_object_or_404(Item.objects.select_for_update(), code=code, vendor=vendor, box__isnull=True)
 
     if settings.KIRPPU_COPY_ITEM_WHEN_UNPRINTED:
         # Create a duplicate of the item with a new code and hide the old item.
@@ -146,7 +146,7 @@ def item_to_not_printed(request, code):
     else:
         item.printed = False
         new_item = item
-    item.save()
+    item.save(update_fields=("hidden", "printed"))
 
     item_dict = {
         'vendor_id': new_item.vendor_id,
@@ -165,10 +165,10 @@ def item_to_not_printed(request, code):
 @require_http_methods(["POST"])
 def item_to_printed(request, code):
     vendor = Vendor.get_vendor(request)
-    item = get_object_or_404(Item.objects, code=code, vendor=vendor, box__isnull=True)
+    item = get_object_or_404(Item.objects.select_for_update(), code=code, vendor=vendor, box__isnull=True)
 
     item.printed = True
-    item.save()
+    item.save(update_fields=("printed",))
 
     return HttpResponse()
 
@@ -183,13 +183,13 @@ def item_update_price(request, code):
         return HttpResponseBadRequest(u' '.join(error.messages))
 
     vendor = Vendor.get_vendor(request)
-    item = get_object_or_404(Item.objects, code=code, vendor=vendor, box__isnull=True)
+    item = get_object_or_404(Item.objects.select_for_update(), code=code, vendor=vendor, box__isnull=True)
 
     if item.is_locked():
         return HttpResponseBadRequest("Item has been brought to event. Price can't be changed.")
 
     item.price = str(price)
-    item.save()
+    item.save(update_fields=("price",))
 
     return HttpResponse(str(price).replace(".", ","))
 
@@ -203,13 +203,13 @@ def item_update_name(request, code):
     name = name[:80]
 
     vendor = Vendor.get_vendor(request)
-    item = get_object_or_404(Item.objects, code=code, vendor=vendor)
+    item = get_object_or_404(Item.objects.select_for_update(), code=code, vendor=vendor)
 
     if item.is_locked():
         return HttpResponseBadRequest("Item has been brought to event. Name can't be changed.")
 
     item.name = name
-    item.save()
+    item.save(update_fields=("name",))
 
     return HttpResponse(name)
 
@@ -220,9 +220,9 @@ def item_update_type(request, code):
     tag_type = request.POST.get("tag_type", None)
 
     vendor = Vendor.get_vendor(request)
-    item = get_object_or_404(Item.objects, code=code, vendor=vendor)
+    item = get_object_or_404(Item.objects.select_for_update(), code=code, vendor=vendor)
     item.type = tag_type
-    item.save()
+    item.save(update_fields=("type",))
     return HttpResponse()
 
 
@@ -845,7 +845,7 @@ def accept_terms(request):
     vendor = Vendor.get_or_create_vendor(request)
     if vendor.terms_accepted is None:
         vendor.terms_accepted = timezone.now()
-        vendor.save()
+        vendor.save(update_fields=("terms_accepted",))
 
     result = timezone.template_localtime(vendor.terms_accepted)
     result = localize(result)
@@ -864,7 +864,7 @@ def remove_item_from_receipt(request):
     form = get_form(ItemRemoveForm, request)
 
     if request.method == "POST" and form.is_valid():
-        form.save(request)
+        _remove_item_from_receipt(request, form.cleaned_data["code"], form.cleaned_data["receipt"])
         return HttpResponseRedirect(url.reverse('kirppu:remove_item_from_receipt'))
 
     return render(request, "kirppu/app_item_receipt_remove.html", {

commit 6cb0b74472ce78bafd00be89879286a29044e026

Fix barcode validation in item removal form.
Field was previously renamed but the function name was not. Regression
from 3c8ea31656a44c077e8e24faed9ecbe210d91b90.
Improve error message if given value is not of expected type.

kirppu/forms.py
@@ -298,8 +298,10 @@ class ItemRemoveForm(forms.Form):
             raise forms.ValidationError(u"Receipt {pk} not found.".format(pk=data))
         return data
 
-    def clean_item(self):
+    def clean_code(self):
         data = self.cleaned_data["code"]
+        if not Item.is_item_barcode(data):
+            raise forms.ValidationError("Value is not an item barcode")
         if not Item.objects.filter(code=data, vendor__event=self._event).exists():
             raise forms.ValidationError(u"Item with code {code} not found.".format(code=data))
         return data