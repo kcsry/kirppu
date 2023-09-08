kcsry/kirppu

commit 02c54c0f3df17978f0c040632143348a78d98c95

Rework how item edit dialog is used.
Refer directly to the values in the template.

kirppu/static_src/jst/item_edit_dialog.jsx
@@ -1,4 +1,5 @@
 import {Options, PriceField} from './form_fields.jsx';
+import VendorInfo from './vendor_info.jsx';
 
 function Header() {
     return (
@@ -11,7 +12,7 @@ function Header() {
     )
 }
 
-function Form({CURRENCY, item_types, item_states}) {
+function Form({CURRENCY, item_types, item_states, item}) {
     return (
         <form className="form-horizontal">
             <div className="form-group">
@@ -23,6 +24,7 @@ function Form({CURRENCY, item_types, item_states}) {
                     <input id="item-edit-name-input"
                            type="text"
                            className="form-control"
+                           value={item.name}
                            readOnly/>
                 </div>
             </div>
@@ -35,12 +37,15 @@ function Form({CURRENCY, item_types, item_states}) {
                     <input id="item-edit-code-input"
                            type="text"
                            className="form-control receipt-code"
+                           value={item.code}
                            readOnly/>
                 </div>
             </div>
             <div className="form-group">
                 <label className="col-sm-2 control-label">{gettext("Vendor")}</label>
-                <div id="item-edit-vendor-info" className="col-sm-10"/>
+                <div className="col-sm-10">
+                    <VendorInfo vendor={item.vendor} title={false} />
+                </div>
             </div>
             <div className="form-group">
                 <div className="col-sm-10 col-sm-offset-2">
@@ -73,6 +78,7 @@ function Form({CURRENCY, item_types, item_states}) {
                              className="form-control"
                              disabled
                              list={item_types}
+                             selection={item.itemtype}
                     />
                 </div>
             </div>
@@ -85,6 +91,7 @@ function Form({CURRENCY, item_types, item_states}) {
                     <Options id="item-edit-state-input"
                              className="form-control"
                              list={item_states}
+                             selection={item.state}
                     />
                 </div>
             </div>
@@ -100,6 +107,7 @@ function Form({CURRENCY, item_types, item_states}) {
                                name="item-edit-abandoned-input"
                                type="radio"
                                value="true"
+                               checked={item.abandoned}
                                disabled/>
                         {gettext("Yes")}
                     </label>
@@ -109,6 +117,7 @@ function Form({CURRENCY, item_types, item_states}) {
                                name="item-edit-abandoned-input"
                                type="radio"
                                value="false"
+                               checked={!item.abandoned}
                                disabled/>
                         {gettext("No")}
                     </label>
@@ -142,37 +151,42 @@ function PrintFrame() {
 }
 
 
-export default function render({CURRENCY, item_types, item_states}) {
+export function item_edit_dialog_modal() {
     return (
         <div className="modal fade">
-            <div className="modal-dialog">
-                <div className="modal-content">
-                    <Header/>
-                    <div className="modal-body">
-                        <div className="container-fluid">
-                            <Form CURRENCY={CURRENCY} item_types={item_types} item_states={item_states}/>
-                            <PrintFrame/>
-                        </div>
-                    </div>
-                    <div id="item-edit-error"
-                         role="alert"
-                         className="alert alert-danger alert-off"/>
-                    <div className="modal-footer">
-                        <button className="btn btn-default btn-minwidth"
-                                data-dismiss="modal">
-                            {gettext("Cancel")}
-                        </button>
-                        <button id="item-edit-print-button"
-                                className="btn btn-primary btn-minwidth">
-                            {gettext("Print")}
-                        </button>
-                        <button id="item-edit-save-button"
-                                className="btn btn-primary btn-minwidth"
-                                disabled>
-                            {gettext("Save")}
-                        </button>
-                    </div>
+            <div className="modal-dialog"/>
+        </div>
+    )
+}
+
+export function item_edit_dialog_content({CURRENCY, item_types, item_states, item, onPrint}) {
+    return (
+        <div className="modal-content">
+            <Header/>
+            <div className="modal-body">
+                <div className="container-fluid">
+                    <Form CURRENCY={CURRENCY} item_types={item_types} item_states={item_states} item={item}/>
+                    <PrintFrame/>
                 </div>
+            </div>
+            <div id="item-edit-error"
+                 role="alert"
+                 className="alert alert-danger alert-off"/>
+            <div className="modal-footer">
+                <button className="btn btn-default btn-minwidth"
+                        data-dismiss="modal">
+                    {gettext("Cancel")}
+                </button>
+                <button id="item-edit-print-button"
+                        className="btn btn-primary btn-minwidth"
+                        onClick={onPrint}>
+                    {gettext("Print")}
+                </button>
+                <button id="item-edit-save-button"
+                        className="btn btn-primary btn-minwidth"
+                        disabled>
+                    {gettext("Save")}
+                </button>
             </div>
         </div>
     )

kirppu/static_src/js/overseer/item_edit_dialog.coffee
@@ -6,23 +6,26 @@ class @ItemEditDialog
     @item = item
     @action = action
 
-    dialog = $(Template.item_edit_dialog(
+    @dialog = $(Template.item_edit_dialog_modal())
+    @dialog.on('shown.bs.modal', => do @onShown)
+    @dialog.on('hidden.bs.modal', => do @dialog.remove)
+    @setItem(item)
+
+  setItem: (item) =>
+    dialog = $(Template.item_edit_dialog_content(
       CURRENCY: CURRENCY.raw
       item_types: ItemSearchForm.itemtypes
       item_states: ItemSearchForm.itemstates
+      item: item
+      onPrint: @onPrint
     ))
+    @dialog.find(".modal-dialog").empty().append(dialog)
 
-    @typeInput = dialog.find('#item-edit-type-input')
-    @stateInput = dialog.find('#item-edit-state-input')
-    @nameInput = dialog.find('#item-edit-name-input')
-    @codeInput = dialog.find('#item-edit-code-input')
+    @form = dialog.find("form")[0]
     @priceInput = dialog.find('#item-edit-price-input')
-    @abandonedYes = dialog.find('#item-edit-abandoned-yes')
-    @abandonedNo = dialog.find('#item-edit-abandoned-no')
     @priceConfirm = dialog.find('#item-edit-price-confirm')
     @errorDiv = dialog.find('#item-edit-error')
     @saveButton = dialog.find('#item-edit-save-button')
-    @printButton = dialog.find('#item-edit-print-button')
 
     @priceConfirm.change(=>
       if @priceConfirm.prop('checked')
@@ -34,40 +37,25 @@ class @ItemEditDialog
 
     dialog.find('input').change(@onChange).on("keyup", @onChange)
     dialog.find('select').change(@onChange)
-    dialog.on('hidden.bs.modal', -> do dialog.remove)
-    dialog.on('shown.bs.modal', => do @onShown)
-    @dialog = dialog
 
     @priceTag = $('.item_template').clone()
       .removeClass('item_template')
       .addClass('item_short')
 
     @saveButton.click(@onSave)
-    @printButton.click(@onPrint)
 
-    @setItem(item)
-
-  setItem: (item) =>
     @item = item
 
     do @updatePriceTag
 
-    @dialog.find('#item-edit-vendor-info').empty().append(
-      Template.vendor_info(vendor: item.vendor, title: false)
-    )
-    @nameInput.val(item.name)
-    @codeInput.val(item.code)
     @priceInput.val(item.price.formatCents())
-    @typeInput.val(item.itemtype)
-    @stateInput.val(item.state)
-    if item.abandoned
-      @abandonedYes.prop('checked', true)
-    else
-      @abandonedNo.prop('checked', true)
 
     @priceConfirm.prop('checked', false)
     @priceConfirm.change()
     @saveButton.prop('disabled', true)
+
+    # Ensure we have any value for all required fields.
+    @getFormState()
     return
 
   updatePriceTag: =>
@@ -105,8 +93,7 @@ class @ItemEditDialog
       @errorDiv.addClass('alert-off')
 
   onChange: =>
-    # Enable or disable save button depending on wheter any values have
-    # been changed.
+    # Enable or disable save button depending on whether any values have been changed.
     if do @hasChanged
       @saveButton.prop('disabled', false)
     else
@@ -136,11 +123,17 @@ class @ItemEditDialog
     do frame.window.focus
     do frame.window.print
 
+  getFromForm: (id) =>
+    for i in @form.elements
+      if i.id == id
+        return i
+    return null
+
   getFormState: =>
     code: @item.code
 
-    name: @nameInput.val()
-    price: @priceInput.val()
-    itemtype: @typeInput.val()
-    state: @stateInput.val()
-    abandoned: @abandonedYes.prop('checked')
+    name: @getFromForm("item-edit-name-input").value
+    price: @getFromForm("item-edit-price-input").value
+    itemtype: @getFromForm("item-edit-type-input").value
+    state: @getFromForm("item-edit-state-input").value
+    abandoned: @getFromForm("item-edit-abandoned-yes").checked

kirppu/static_src/jst/form_fields.jsx
@@ -1,10 +1,10 @@
 
-export function Options({list, multiple}) {
+export function Options({list, multiple, selection}) {
     // multiple must be set here, otherwise the control will half-select first item by default.
     return (
         <select multiple={multiple} className="form-control">{
             list.map(t => {
-                return <option value={t.name}>{t.description}</option>
+                return <option value={t.name} selected={t.name === selection}>{t.description}</option>
             })
         }</select>
     )

kirppu/static_src/jst/index.js
@@ -1,7 +1,7 @@
 import box_check_in_dialog from "./box_check_in_dialog.jsx";
 import box_report_table from "./box_report_table.jsx";
 import box_sell_allocation_dialog from "./box_sell_allocation_dialog.jsx";
-import item_edit_dialog from './item_edit_dialog.jsx';
+import {item_edit_dialog_content, item_edit_dialog_modal} from './item_edit_dialog.jsx';
 import {item_find_table, item_find_table_item, item_find_table_no_results} from "./item_find_table.jsx";
 import {item_receipt_table, item_receipt_table_row} from './item_receipt_table.jsx';
 import item_report_table from "./item_report_table.jsx";
@@ -23,7 +23,8 @@ const Template = {
     box_check_in_dialog: box_check_in_dialog,
     box_report_table: box_report_table,
     box_sell_allocation_dialog: box_sell_allocation_dialog,
-    item_edit_dialog: item_edit_dialog,
+    item_edit_dialog_content: item_edit_dialog_content,
+    item_edit_dialog_modal: item_edit_dialog_modal,
     item_find_table: item_find_table,
     item_find_table_item: item_find_table_item,
     item_find_table_no_results: item_find_table_no_results,

commit 02f424243ea6ba7df4d6eeccc372e04908cd7cc6

Fix item edit Print button behavior.
Probably regression from 02c54c0f3df17978f0c040632143348a78d98c95.

kirppu/static_src/jst/item_edit_dialog.jsx
@@ -223,7 +223,7 @@ export function item_edit_dialog_content({CURRENCY, item_types, item_states, ite
                 </button>
                 <button id="item-edit-print-button"
                         className="btn btn-primary btn-minwidth"
-                        onClick={onPrint}>
+                        onclick={() => onPrint()}>
                     {gettext("Print")}
                 </button>
                 <button id="item-edit-save-button"