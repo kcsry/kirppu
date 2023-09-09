4014ba161ff265e9ae3041373603918cdf5b4ce4
Jyrki Launonen <jyrki.launonen@gmail.com>
2023-09-09 17:19:34 +0300

    Increase item search form label width a bit.

--- kirppu/static_src/jst/item_search_form.jsx
+++ kirppu/static_src/jst/item_search_form.jsx
@@ -8,56 +8,56 @@ export default function render({item_types, item_states, price_step, CURRENCY}) 
                 {"form.item-search-form select { height: 9.5em; }"}
             </style>
             <div className="form-group">
-                <label htmlFor="item_search_input" className="control-label col-sm-2">{gettext("Name / Bar code")}</label>
-                <div className="input-group col-sm-10">
+                <label htmlFor="item_search_input" className="control-label col-sm-3">{gettext("Name / Bar code")}</label>
+                <div className="input-group col-sm-9">
                     <input type="text" id="item_search_input" className="form-control"/>
                 </div>
             </div>
             <div className="form-group">
                 <label htmlFor="item_code_search_input"
-                       className="control-label col-sm-2">{gettext("Bar code part")}</label>
-                <div className="input-group col-sm-10">
+                       className="control-label col-sm-3">{gettext("Bar code part")}</label>
+                <div className="input-group col-sm-9">
                     <input type="text" id="item_code_search_input" className="form-control"/>
                 </div>
             </div>
             <div className="form-group">
                 <label htmlFor="box_number_input"
-                       className="control-label col-sm-2">{gettext("Box number")}</label>
+                       className="control-label col-sm-3">{gettext("Box number")}</label>
                 <div className="input-group col-sm-2">
                     <input type="text" id="box_number_input" className="form-control"/>
                 </div>
             </div>
             <div className="form-group">
-                <label htmlFor="vendor_search_input" className="control-label col-sm-2">{gettext("Vendor ID")}</label>
+                <label htmlFor="vendor_search_input" className="control-label col-sm-3">{gettext("Vendor ID")}</label>
                 <div className="input-group col-sm-2">
                     <input type="number" step="1" min="1" id="vendor_search_input" className="form-control"/>
                 </div>
             </div>
             <div className="form-group form-inline">
                 <label htmlFor="item_search_min_price"
-                       className="control-label col-sm-2">{gettext("Price")}</label>
+                       className="control-label col-sm-3">{gettext("Price")}</label>
                 <PriceField inputId="item_search_min_price" className="input-group col-sm-2" price_step={price_step} CURRENCY={CURRENCY} placeholder={gettext("Min")}/>
                 {" - "}
                 <PriceField inputId="item_search_max_price" className="input-group col-sm-2" price_step={price_step} CURRENCY={CURRENCY} placeholder={gettext("Max")}/>
             </div>
             <div className="form-group">
-                <label htmlFor="item_search_type" className="control-label col-sm-2">{gettext("Type")}</label>
-                <div className="input-group col-sm-10">
+                <label htmlFor="item_search_type" className="control-label col-sm-3">{gettext("Type")}</label>
+                <div className="input-group col-sm-9">
                     <Options multiple id="item_search_type" list={item_types}/>
                     <span
                         className="help-block">{html(gettext("To select/deselect multiple values, press <kbd>Ctrl</kbd>, <kbd>Command</kbd> or <kbd>Shift</kbd> keys while clicking items."))}</span>
                 </div>
             </div>
             <div className="form-group">
-                <label htmlFor="item_search_state" className="control-label col-sm-2">{gettext("State")}</label>
-                <div className="input-group col-sm-10">
+                <label htmlFor="item_search_state" className="control-label col-sm-3">{gettext("State")}</label>
+                <div className="input-group col-sm-9">
                     <Options multiple id="item_search_state" list={item_states}/>
                     <span
                         className="help-block">{html(gettext("To select/deselect multiple values, press <kbd>Ctrl</kbd>, <kbd>Command</kbd> or <kbd>Shift</kbd> keys while clicking items."))}</span>
                 </div>
             </div>
             <div className="form-group">
-                <label className="control-label col-sm-2">{gettext("Is a box?")}</label>
+                <label className="control-label col-sm-3">{gettext("Is a box?")}</label>
                 <label className="radio-inline">
                     <input type="radio" id="show_box_na" defaultChecked="defaultChecked" name="is_box" value="" checked="checked"/>{gettext("Don't care")}
                 </label>
@@ -69,7 +69,7 @@ export default function render({item_types, item_states, price_step, CURRENCY}) 
                 </label>
             </div>
             <div className="form-group">
-                <div className="input-group col-sm-offset-2 col-sm-10">
+                <div className="input-group col-sm-offset-3 col-sm-9">
                     <div className="checkbox">
                         <label>
                             <input type="checkbox" id="show_hidden_items"/>
@@ -78,7 +78,7 @@ export default function render({item_types, item_states, price_step, CURRENCY}) 
                     </div>
                 </div>
             </div>
-            <div className="col-sm-offset-2">
+            <div className="col-sm-offset-3">
                 <button type="submit" className="btn btn-primary btn-minwidth">{gettext("Search")}</button>
                 {" "}
                 <button type="reset" className="btn btn-default btn-minwidth">{gettext("Reset")}</button>
